d9c2b3f38a117282232e9b35af9f07202aa2b622
aalvarez <aalvarez@4525493e-7705-40b1-a816-d608a930855b>
2012-01-26 12:47:17 +0000

    Added missing files

---
+++ dmlite/trunk/include/dmlite/dm_pool.h
@@ -0,0 +1,63 @@
+/// @file   include/dm/dm_pool.h
+/// @brief  Pool API.
+/// @author Alejandro Álvarez Ayllón <aalvarez@cern.ch>
+#ifndef DMLITE_POOL_H
+#define	DMLITE_POOL_H
+
+#include <string>
+#include <vector>
+#include "dm_auth.h"
+#include "dm_exceptions.h"
+#include "dm_types.h"
+
+namespace dmlite {
+
+/// Interface for pool types.
+class PoolManager: public AuthBase {
+public:
+  /// Destructor.
+  virtual ~PoolManager();
+
+  /// String ID of the pool implementation.
+  virtual std::string getImplId(void) throw() = 0;
+
+  /// Get the list of pools.
+  /// @return A set with all the pools.
+  virtual std::vector<Pool> getPools(void) throw (DmException) = 0;
+
+  /// Get the list of filesystems in a pool.
+  /// @param poolname The pool name.
+  /// @return         A set with the filesystems that belong to the pool.
+  virtual std::vector<FileSystem> getPoolFilesystems(const std::string& poolname) throw (DmException) = 0;
+
+  /// Get the status of a filesystem within a pool.
+  /// @param pool   The pool name.
+  /// @param server The disk server.
+  /// @param fs     The file system within the server.
+  /// @return       A struct associated with the requested filesystem.
+  virtual FileSystem getFilesystem(const std::string& pool, const std::string& server,
+                                   const std::string& fs) throw(DmException) = 0;
+};
+
+/// Plug-ins must implement a concrete factory to be instantiated.
+class PoolManagerFactory {
+public:
+  /// Virtual destructor
+  virtual ~PoolManagerFactory();
+
+  /// Set a configuration parameter
+  /// @param key   The configuration parameter
+  /// @param value The value for the configuration parameter
+  virtual void configure(const std::string& key, const std::string& value) throw (DmException) = 0;
+
+  /// Instantiate a implementation of Pool
+  virtual PoolManager* createPoolManager() throw (DmException) = 0;
+
+protected:
+private:
+};
+
+
+};
+
+#endif	// DMLITE_POOL_H
---
+++ dmlite/trunk/plugins/profiler/ProfilerPoolManager.h
@@ -0,0 +1,34 @@
+/// @file    plugins/profiler/ProfilerPoolManager.h
+/// @brief   Profiler plugin.
+/// @author  Alejandro Álvarez Ayllón <aalvarez@cern.ch>
+#ifndef PROFILERPOOLMANAGER_H
+#define	PROFILERPOOLMANAGER_H
+
+#include <dmlite/dm_pool.h>
+
+namespace dmlite {
+
+class ProfilerPoolManager: public PoolManager {
+public:
+  ProfilerPoolManager(PoolManager* decorated) throw (DmException);
+  ~ProfilerPoolManager();
+
+  std::string getImplId(void) throw();
+
+  std::vector<Pool>       getPools          (void)               throw (DmException);
+  std::vector<FileSystem> getPoolFilesystems(const std::string&) throw (DmException);
+  FileSystem              getFilesystem      (const std::string&, const std::string&, const std::string&) throw (DmException);
+
+  void setUserId  (uid_t, gid_t, const std::string&) throw (DmException);
+  void setVomsData(const std::string&, const std::vector<std::string>&) throw (DmException);
+
+protected:
+  PoolManager* decorated_;
+  char*        decoratedId_;
+private:
+};
+
+};
+
+#endif	// PROFILERPOOLMANAGER_H
+
