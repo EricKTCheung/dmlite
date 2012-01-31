000b0b6ee11fc583e90f98e8898353bcb682a01f
aalvarez <aalvarez@4525493e-7705-40b1-a816-d608a930855b>
2012-01-31 13:31:16 +0000

    create deals better with files that exist

--- dmlite/trunk/tests/test-create.cpp
+++ dmlite/trunk/tests/test-create.cpp
@@ -11,6 +11,7 @@ protected:
   const static char *FOLDER;
   const static char *NESTED;
   const static char *SYMLINK;
+  const static char *FILE;
 
 public:
 
@@ -22,6 +23,11 @@ public:
 
   void tearDown()
   {
+    try {
+      this->catalog->unlink(FILE);
+    }
+    catch (...) { }
+
     try {
       this->catalog->unlink(SYMLINK);
     }
@@ -47,19 +53,33 @@ public:
     CPPUNIT_ASSERT_EQUAL(MODE | S_IFDIR, (int)s.st_mode);
     ini_mtime = s.st_mtime;
     sleep(1); // Or mtime may be the same, since the operation can be fast
+
     // Add a folder
     this->catalog->makeDir(NESTED, MODE);
     s = this->catalog->stat(FOLDER);
     CPPUNIT_ASSERT_EQUAL(1, (int)s.st_nlink);
     CPPUNIT_ASSERT(s.st_mtime > ini_mtime);
+
     // Add a symlink
     this->catalog->symlink(FOLDER, SYMLINK);
     s = this->catalog->stat(FOLDER);
     CPPUNIT_ASSERT_EQUAL(2, (int)s.st_nlink);
+    
+    // Add a file
+    this->catalog->create(FILE, MODE);
+    s = this->catalog->stat(FOLDER);
+    CPPUNIT_ASSERT_EQUAL(3, (int)s.st_nlink);
+    
+    // Remove the file
+    this->catalog->unlink(FILE);
+    s = this->catalog->stat(FOLDER);
+    CPPUNIT_ASSERT_EQUAL(2, (int)s.st_nlink);
+
     // Remove the folder
     this->catalog->removeDir(NESTED);
     s = this->catalog->stat(FOLDER);
     CPPUNIT_ASSERT_EQUAL(1, (int)s.st_nlink);
+
     // And the symlink
     this->catalog->unlink(SYMLINK);
     s = this->catalog->stat(FOLDER);
@@ -95,10 +115,41 @@ public:
     this->catalog->removeDir(NESTED);
   }
 
+  void testExistsNoReplicas()
+  {
+    struct stat s;
+
+    this->catalog->create(FILE, MODE);
+    // Again, should not fail
+    this->catalog->create(FILE, MODE);
+
+    s = this->catalog->stat(FILE);
+    CPPUNIT_ASSERT_EQUAL(0l, (long)s.st_size);
+  }
+
+  void testExistsWithReplicas()
+  {
+    struct stat s;
+
+    this->catalog->create(FILE, MODE);
+    s = this->catalog->stat(FILE);
+
+    this->catalog->addReplica("", s.st_ino,
+                              "localhost", "sfn://something",
+                              '-', 'P',
+                              "", "");
+
+    CPPUNIT_ASSERT_THROW(this->catalog->create(FILE, MODE), dmlite::DmException);
+
+    this->catalog->deleteReplica("", s.st_ino, "sfn://something");
+  }
+
   CPPUNIT_TEST_SUITE(TestCreate);
   CPPUNIT_TEST(testRegular);
   CPPUNIT_TEST(testSetGid);
   CPPUNIT_TEST(testUmask);
+  CPPUNIT_TEST(testExistsNoReplicas);
+  CPPUNIT_TEST(testExistsWithReplicas);
   CPPUNIT_TEST_SUITE_END();
 };
 
@@ -106,6 +157,7 @@ const int   TestCreate::MODE    = 0700;
 const char* TestCreate::FOLDER  = "/dpm/cern.ch/home/dteam/test-create";
 const char* TestCreate::NESTED  = "/dpm/cern.ch/home/dteam/test-create/nested";
 const char* TestCreate::SYMLINK = "/dpm/cern.ch/home/dteam/test-create/test-link";
+const char* TestCreate::FILE    = "/dpm/cern.ch/home/dteam/test-create/test-file";
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestCreate);
 
