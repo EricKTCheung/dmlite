1a25efe7b4d79d54e800abcc8602a0543d064ff5
aalvarez <aalvarez@4525493e-7705-40b1-a816-d608a930855b>
2012-02-02 13:09:22 +0000

    Fixes to assure NULL is not passed to std::string constructor

--- core/Catalog-C.cpp
+++ core/Catalog-C.cpp
@@ -48,11 +48,20 @@ try {\
 
 
 
+/// Safe strings (empty instead of NULL)
+#define SAFE_STRING(s) s==NULL?"":s
+
+
+/// Throw an exception if the string is NULL
+#define NOT_NULL(s) if (s==NULL) throw dmlite::DmException(DM_NULL_POINTER, #s" is a NULL pointer")
+
+
 int dm_set(dm_context *context, const char *key, ...)
 {
   va_list vargs;
 
   TRY(context, set)
+  NOT_NULL(key);
   va_start(vargs, key);
   context->catalog->set(key, vargs);
   va_end(vargs);
@@ -64,6 +73,7 @@ int dm_set(dm_context *context, const char *key, ...)
 int dm_chdir(dm_context* context, const char* path)
 {
   TRY(context, chdir)
+  NOT_NULL(path);
   context->catalog->changeDir(path);
   CATCH(context, chdir)
 }
@@ -86,6 +96,7 @@ char* dm_getcwd(dm_context* context, char* buffer, size_t size)
 int dm_stat(dm_context *context, const char* path, struct stat* buf)
 {
   TRY(context, stat)
+  NOT_NULL(path);
   *buf = context->catalog->stat(path);
   CATCH(context, stat)
 }
@@ -95,6 +106,8 @@ int dm_stat(dm_context *context, const char* path, struct stat* buf)
 int dm_lstat(dm_context* context, const char* path, struct stat* buf)
 {
   TRY(context, lstat)
+  NOT_NULL(path);
+  NOT_NULL(buf);
   *buf = context->catalog->linkStat(path);
   CATCH(context, lstat);
 }
@@ -104,6 +117,8 @@ int dm_lstat(dm_context* context, const char* path, struct stat* buf)
 int dm_xstat(dm_context* context, const char* path, struct xstat* buf)
 {
   TRY(context, lstat)
+  NOT_NULL(path);
+  NOT_NULL(buf);
   *buf = context->catalog->extendedStat(path);
   CATCH(context, lstat);
 }
@@ -113,6 +128,7 @@ int dm_xstat(dm_context* context, const char* path, struct xstat* buf)
 int dm_istat(dm_context* context, ino_t inode, struct stat* buf)
 {
   TRY(context, lstat)
+  NOT_NULL(buf);
   *buf = context->catalog->stat(inode);
   CATCH(context, lstat);
 }
@@ -122,6 +138,7 @@ int dm_istat(dm_context* context, ino_t inode, struct stat* buf)
 int dm_ixstat(dm_context* context, ino_t inode, struct xstat* buf)
 {
   TRY(context, lstat)
+  NOT_NULL(buf);
   *buf = context->catalog->extendedStat(inode);
   CATCH(context, lstat);
 }
@@ -134,7 +151,11 @@ int dm_addreplica(dm_context* context, const char* guid, int64_t id,
                   const char* fileSystem)
 {
   TRY(context, addReplica)
-  context->catalog->addReplica(guid, id, server, path, status, fileType, poolName, fileSystem);
+  NOT_NULL(server);
+  NOT_NULL(path);
+  context->catalog->addReplica(SAFE_STRING(guid), id, server,
+                               path, status, fileType,
+                               SAFE_STRING(poolName), SAFE_STRING(fileSystem));
   CATCH(context, addReplica)
 }
 
@@ -144,10 +165,8 @@ int dm_delreplica(dm_context* context, const char* guid, int64_t id,
                   const char* path)
 {
   TRY(context, delreplica)
-  if (guid != NULL)
-    context->catalog->deleteReplica(guid, id, path);
-  else
-    context->catalog->deleteReplica(std::string(), id, path);
+  NOT_NULL(path);
+  context->catalog->deleteReplica(SAFE_STRING(guid), id, path);
   CATCH(context, delreplica)
 }
 
@@ -157,6 +176,9 @@ int dm_getreplicas(dm_context* context, const char* path, int *nEntries,
                   struct filereplica** fileReplicas)
 {
   TRY(context, getreplicas)
+  NOT_NULL(path);
+  NOT_NULL(nEntries);
+  NOT_NULL(fileReplicas);
 
   std::vector<filereplica> replicaSet = context->catalog->getReplicas(path);
 
@@ -185,6 +207,8 @@ int dm_freereplicas(dm_context* context, int nReplicas, struct filereplica* file
 int dm_get(dm_context* context, const char* path, struct filereplica* replica)
 {
   TRY(context, get)
+  NOT_NULL(path);
+  NOT_NULL(replica);
   *replica = context->catalog->get(path);
   CATCH(context, get)
 }
@@ -194,6 +218,9 @@ int dm_get(dm_context* context, const char* path, struct filereplica* replica)
 int dm_put(dm_context* context, const char* path, struct uri* uri, char* token)
 {
   TRY(context, put)
+  NOT_NULL(path);
+  NOT_NULL(uri);
+  NOT_NULL(token);
   std::string t = context->catalog->put(path, uri);
   strcpy(token, t.c_str());
   CATCH(context, put)
@@ -204,6 +231,10 @@ int dm_put(dm_context* context, const char* path, struct uri* uri, char* token)
 int dm_putg(dm_context* context, const char* path, struct uri* uri, const char* guid, char* token)
 {
   TRY(context, putg)
+  NOT_NULL(path);
+  NOT_NULL(uri);
+  NOT_NULL(guid);
+  NOT_NULL(token);
   std::string t;
   if (guid != NULL)
     t = context->catalog->put(path, uri, guid);
@@ -218,6 +249,9 @@ int dm_putg(dm_context* context, const char* path, struct uri* uri, const char* 
 int dm_putstatus(dm_context* context, const char* path, const char* token, struct uri* uri)
 {
   TRY(context, putstatus)
+  NOT_NULL(path);
+  NOT_NULL(token);
+  NOT_NULL(uri);
   context->catalog->putStatus(path, token, uri);
   CATCH(context, putstatus)
 }
@@ -227,6 +261,8 @@ int dm_putstatus(dm_context* context, const char* path, const char* token, struc
 int dm_putdone(dm_context* context, const char* path, const char* token)
 {
   TRY(context, putdone)
+  NOT_NULL(path);
+  NOT_NULL(token);
   context->catalog->putDone(path, token);
   CATCH(context, putdone)
 }
@@ -236,6 +272,7 @@ int dm_putdone(dm_context* context, const char* path, const char* token)
 int dm_unlink(dm_context* context, const char* path)
 {
   TRY(context, unlink)
+  NOT_NULL(path);
   context->catalog->unlink(path);
   CATCH(context, unlink)
 }
@@ -245,6 +282,7 @@ int dm_unlink(dm_context* context, const char* path)
 int dm_chmod(dm_context* context, const char* path, mode_t mode)
 {
   TRY(context, chmod)
+  NOT_NULL(path);
   context->catalog->changeMode(path, mode);
   CATCH(context, chmod)
 }
@@ -254,6 +292,7 @@ int dm_chmod(dm_context* context, const char* path, mode_t mode)
 int dm_chown(dm_context* context, const char* path, uid_t newUid, gid_t newGid)
 {
   TRY(context, chown)
+  NOT_NULL(path);
   context->catalog->changeOwner(path, newUid, newGid);
   CATCH(context, chown)
 }
@@ -263,6 +302,7 @@ int dm_chown(dm_context* context, const char* path, uid_t newUid, gid_t newGid)
 int dm_lchown(dm_context* context, const char* path, uid_t newUid, gid_t newGid)
 {
   TRY(context, lchown)
+  NOT_NULL(path);
   context->catalog->linkChangeOwner(path, newUid, newGid);
   CATCH(context, lchown)
 }
@@ -272,6 +312,8 @@ int dm_lchown(dm_context* context, const char* path, uid_t newUid, gid_t newGid)
 int dm_getcomment(dm_context* context, const char* path, char* comment)
 {
   TRY(context, getcomment)
+  NOT_NULL(path);
+  NOT_NULL(comment);
   std::string c = context->catalog->getComment(path);
   strcpy(comment, c.c_str());
   CATCH(context, getcomment)
@@ -282,6 +324,8 @@ int dm_getcomment(dm_context* context, const char* path, char* comment)
 int dm_setcomment(dm_context* context, const char* path, const char* comment)
 {
   TRY(context, setcomment)
+  NOT_NULL(path);
+  NOT_NULL(comment);
   context->catalog->setComment(path, comment);
   CATCH(context, setcomment)
 }
@@ -292,6 +336,7 @@ int dm_setcomment(dm_context* context, const char* path, const char* comment)
 int dm_getgrpbygid(dm_context* context, gid_t gid, char* groupName)
 {
   TRY(context, getgrpbygid)
+  NOT_NULL(groupName);
   groupinfo group = context->catalog->getGroup(groupName);
   strcpy(groupName, group.name);
   CATCH(context, getgrpbygid)
@@ -302,6 +347,8 @@ int dm_getgrpbygid(dm_context* context, gid_t gid, char* groupName)
 int dm_getgrpbynam(dm_context* context, const char* groupName, gid_t* gid)
 {
   TRY(context, getgrpbynam)
+  NOT_NULL(groupName);
+  NOT_NULL(gid);
   groupinfo group = context->catalog->getGroup(groupName);
   *gid = group.gid;
   CATCH(context, getgrpbynam)
@@ -313,13 +360,18 @@ int dm_getidmap(dm_context* context, const char* username, int nGroups,
                 const char** groupNames, uid_t* uid, gid_t* gids)
 {
   TRY(context, getidmap)
+  NOT_NULL(username);
+  if (nGroups > 0) NOT_NULL(groupNames);
+  NOT_NULL(uid);
+  NOT_NULL(gids);
+
   std::vector<std::string> groupSet;
   std::vector<gid_t>       gidSet;
 
   for(int i = 0; i < nGroups; ++i)
     groupSet.push_back(groupNames[i]);
 
-  context->catalog->getIdMap(std::string(username), groupSet, uid, &gidSet);
+  context->catalog->getIdMap(username, groupSet, uid, &gidSet);
 
   int i;
   std::vector<gid_t>::const_iterator j;
@@ -334,6 +386,7 @@ int dm_getidmap(dm_context* context, const char* username, int nGroups,
 int dm_getusrbynam(dm_context* context, const char* userName, uid_t* uid)
 {
   TRY(context, getusrbyuid)
+  NOT_NULL(userName);
   userinfo user = context->catalog->getUser(userName);
   *uid = user.uid;
   CATCH(context, getusrbyuid)
@@ -344,6 +397,7 @@ int dm_getusrbynam(dm_context* context, const char* userName, uid_t* uid)
 int dm_getusrbyuid(dm_context* context, uid_t uid, char* userName)
 {
   TRY(context, getusrbyuid)
+  NOT_NULL(userName);
   userinfo user = context->catalog->getUser(uid);
   strcpy(userName, user.name);
   CATCH(context, getusrbyuid)
@@ -354,6 +408,7 @@ int dm_getusrbyuid(dm_context* context, uid_t uid, char* userName)
 void* dm_opendir(dm_context* context, const char* path)
 {
   TRY(context, opendir)
+  NOT_NULL(path);
   return context->catalog->openDir(path);
   CATCH_POINTER(context, opendir)
 }
@@ -363,6 +418,7 @@ void* dm_opendir(dm_context* context, const char* path)
 int dm_closedir(dm_context* context, void* dir)
 {
   TRY(context, closedir)
+  NOT_NULL(dir);
   context->catalog->closeDir(dir);
   CATCH(context, closedir)
 }
@@ -372,6 +428,7 @@ int dm_closedir(dm_context* context, void* dir)
 struct dirent* dm_readdir(dm_context* context, void* dir)
 {
   TRY(context, readdir)
+  NOT_NULL(dir);
   return context->catalog->readDir(dir);
   CATCH_POINTER(context, readdir)
 }
@@ -381,6 +438,7 @@ struct dirent* dm_readdir(dm_context* context, void* dir)
 struct direntstat* dm_readdirx(dm_context* context, void* dir)
 {
   TRY(context, readdirx)
+  NOT_NULL(dir);
   return context->catalog->readDirx(dir);
   CATCH_POINTER(context, readdirx)
 }
@@ -390,6 +448,7 @@ struct direntstat* dm_readdirx(dm_context* context, void* dir)
 int dm_mkdir(dm_context* context, const char* path, mode_t mode)
 {
   TRY(context, mkdir)
+  NOT_NULL(path);
   context->catalog->makeDir(path, mode);
   CATCH(context, mkdir)
 }
@@ -399,6 +458,8 @@ int dm_mkdir(dm_context* context, const char* path, mode_t mode)
 int dm_rename(dm_context* context, const char* oldPath, const char* newPath)
 {
   TRY(context, rename)
+  NOT_NULL(oldPath);
+  NOT_NULL(newPath);
   context->catalog->rename(oldPath, newPath);
   CATCH(context, rename)
 }
@@ -408,6 +469,7 @@ int dm_rename(dm_context* context, const char* oldPath, const char* newPath)
 int dm_rmdir(dm_context* context, const char* path)
 {
   TRY(context, rmdir)
+  NOT_NULL(path);
   context->catalog->removeDir(path);
   CATCH(context, rmdir)
 }
@@ -417,7 +479,7 @@ int dm_rmdir(dm_context* context, const char* path)
 int dm_setuserid(dm_context* context, uid_t uid, gid_t gid, const char* dn)
 {
   TRY(context, setuserid)
-  context->catalog->setUserId(uid, gid, dn);
+  context->catalog->setUserId(uid, gid, SAFE_STRING(dn));
   CATCH(context, setuserid)
 }
 
@@ -426,6 +488,8 @@ int dm_setuserid(dm_context* context, uid_t uid, gid_t gid, const char* dn)
 int dm_setvomsdata(dm_context* context, const char* vo, const char** fqans, int nFqans)
 {
   TRY(context, setvomsdata)
+  NOT_NULL(vo);
+  if (nFqans > 0) NOT_NULL(fqans);
   std::vector<std::string> fqansSet;
 
   for (int i = 0; i < nFqans; ++i)
@@ -446,6 +510,9 @@ int dm_getpools(dm_context* context, int* nbpools, struct pool** pools)
   }
 
   TRY(context, getpools)
+  NOT_NULL(nbpools);
+  NOT_NULL(pools);
+  
   std::vector<pool> poolSet = context->pool->getPools();
 
   *pools = new pool[poolSet.size()];
@@ -482,6 +549,10 @@ int dm_getpoolfs(dm_context* context, const char *poolname, int *nbfs, struct fi
   }
   
   TRY(context, getpoolfs)
+  NOT_NULL(poolname);
+  NOT_NULL(nbfs);
+  NOT_NULL(fs);
+
   std::vector<filesystem> filesystems = context->pool->getPoolFilesystems(poolname);
 
   *fs = new filesystem[filesystems.size()];
