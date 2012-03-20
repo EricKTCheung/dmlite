1860565eb7f10dbe4a05c1155a6fda6707593d7b
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-03-20 15:27:23 +0000

    Added protocol buffers support: changed dmlite.spec, added cmake Find modules in plugins/memcache/; added caching support for replicas, versioned keys.

--- dmlite/trunk/plugins/memcache/Memcache.h
+++ dmlite/trunk/plugins/memcache/Memcache.h
@@ -1,19 +1,11 @@
 /// @file    plugins/memcache/Memcache.h
 /// @brief   memcached plugin.
 /// @author  Martin Philipp Hellmich <mhellmic@cern.ch>
-#ifndef MEMCACHED_H
-#define	MEMCACHED_H
+#ifndef MEMCACHE_H
+#define	MEMCACHE_H
 
 #include <vector>
 #include <sstream>
-
-//#include <fstream>
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-#include <boost/archive/binary_iarchive.hpp>
-#include <boost/iostreams/stream.hpp>
-#include <boost/iostreams/device/back_inserter.hpp>
 
 #include <libmemcached/memcached.h>
 #include <dmlite/dmlite++.h>
@@ -22,11 +14,9 @@
 #include <dmlite/dummy/Dummy.h>
 #include <dmlite/common/Uris.h>
 
-
 namespace dmlite {
 
 #define DEFAULT_MEMCACHED_EXPIRATION 60
-#define SERIALIZE_VERSION 1
 
 /// Memcache plugin
 class MemcacheCatalog: public DummyCatalog {
@@ -45,7 +35,7 @@ public:
 
   // Overloading 
 	void setUserId(uid_t, gid_t, const std::string&) throw (DmException);
-  void        changeDir    (const std::string&) throw (DmException);
+//  void        changeDir    (const std::string&) throw (DmException);
 
   ExtendedStat extendedStat(const std::string&, bool = true) throw (DmException);
   ExtendedStat extendedStat(ino_t)              throw (DmException);
@@ -61,16 +51,27 @@ public:
 
   void create(const std::string&, mode_t) throw (DmException);
 
-  void   changeMode     (const std::string&, mode_t)       throw (DmException);
-  void   changeOwner    (const std::string&, uid_t, gid_t) throw (DmException);
-  void   linkChangeOwner(const std::string&, uid_t, gid_t) throw (DmException);
-/*
+  void changeMode     (const std::string&, mode_t)       throw (DmException);
+  void changeOwner    (const std::string&, uid_t, gid_t) throw (DmException);
+  void linkChangeOwner(const std::string&, uid_t, gid_t) throw (DmException);
+  void utime(const std::string&, const struct utimbuf*) throw (DmException);
+//  void utime(ino_t, const struct utimbuf*)              throw (DmException);
+  void addReplica(const std::string&, int64_t, const std::string&,
+                  const std::string&, char, char,
+                  const std::string&, const std::string&) throw (DmException);
+
   void deleteReplica(const std::string&, int64_t,
                      const std::string&) throw (DmException);
 
   std::vector<FileReplica> getReplicas(const std::string&) throw (DmException);
+  
   FileReplica              get        (const std::string&) throw (DmException);
-*/
+
+  void replicaSetLifeTime  (const std::string&, time_t) throw (DmException);
+  void replicaSetAccessTime(const std::string&)         throw (DmException);
+  void replicaSetType      (const std::string&, char)   throw (DmException);
+  void replicaSetStatus    (const std::string&, char)   throw (DmException);
+
 protected:
   /// The Memcached connection
   memcached_st* conn_;
@@ -84,16 +85,68 @@ protected:
   /// User secondary groups.
   std::vector<GroupInfo> groups_;
 
-  /// Current working dir (path)
-  std::string cwdPath_;
-
-  /// Current working dir
-  ino_t cwd_;
-
-  /// Get a list of replicas using file id
-  std::vector<FileReplica> getReplicas(ino_t) throw (DmException);
+  /// Get a list of replicas by their memcached key 
+  std::vector<FileReplica> getReplicas(const std::string&, ino_t inode) throw (DmException);
 private:
-	/// Converts a key prefix and an inode into a key for memcached.
+  /* 
+  std::string serializeKey(const std::string& key);
+
+  std::string deserializeKey(const std::string& serialKey);
+
+  std::string serializeBlackKey(const std::string& key);
+*/
+  /// Serialize an ExtendedStat object into a string.
+  /// @param var The object to serialize.
+  /// @return The serialized object as string.
+  std::string serialize(const ExtendedStat& var);
+
+  /// Create an object from a serialized string.
+  /// @param serial_str The serialized object as string.
+  /// @param var The deserialized object.
+  void deserialize(std::string& serial_str, ExtendedStat& var);
+
+  /// Serialize a list of keys (strings) into a string.
+  /// The keys can be either 'white' items or
+  /// 'black' items. The black items form a blacklist which
+  /// should be considered when deserializing the list.
+  /// @param keyList The list of keys.
+  /// @param isWhite. 
+  /// @return The serialized object.
+  std::string serializeList(std::vector<std::string>& keyList, const bool isWhite = true);
+ 
+  /// Deserialize a list of keys.
+  /// This function does not take blacklisted items
+  /// into consideration.
+  /// @param serialList The serialized List as string.
+  /// @return           The List.
+  std::vector<std::string> deserializeList(std::string& serialList);
+
+  /// Deserialize a list of keys.
+  /// This function takes blacklisted item into account
+  /// and subtracts those item from the output list.
+  /// @param serialList The serialized List as string.
+  /// @return           The List.
+  std::vector<std::string> deserializeBlackList(std::string& serialList);
+
+  /// Serialize a FileReplica to a string.
+  /// @param replica    The replica to serialize.
+  /// @return           The serialized object.
+  std::string serializeFileReplica(const FileReplica& replica);
+
+  /// Deserialize a FileReplica.
+  /// @param serialRepl The serialized replica.
+  /// @return           The FileReplica.
+  FileReplica deserializeFileReplica(std::string& serialRepl);
+
+//  std::vector<FileReplica> deserialize(std::vector<std::string>&); 
+
+  /// Convert a memcached key to a versioned key.
+  /// @param version  The version to attach
+  /// @param key      The original key.
+  /// @return         The versioned key.
+  const std::string versionedKeyFromAny(uint64_t version, const std::string key);
+
+  /// Converts a key prefix and an inode into a key for memcached.
 	/// @param preKey key prefix string.
 	/// @param inode  inode number.
 	/// @return				The key as string.
@@ -104,6 +157,12 @@ private:
 	/// @param path   path string.
 	/// @return				The key as string.
 	const std::string keyFromAny(const char* preKey, const std::string& path);
+
+	/// Converts a key prefix and a URI string into a key for memcached.
+	/// @param preKey key prefix string.
+	/// @param uri    URI string.
+	/// @return				The key as string.
+	const std::string keyFromURI(const char* preKey, const std::string& uri);
 	/// Converts a key prefix, the inode of the parent dir and the name of the dile/dir/link into a key for memcached.
 	/// @param preKey key prefix string.
 	/// @param parent inode of parent.
@@ -114,14 +173,62 @@ private:
 	/// Return the value to a given key from memcached.
 	/// @param key   The memcached key as string.
 	/// @return      The value from memcached.
-	const std::string valFromMemcachedKey(const std::string key); 
+	const std::string getValFromMemcachedKey(const std::string key); 
 
+	/// Return the value to a given versioned key from memcached.
+	/// @param key   The memcached key as string.
+	/// @return      The value from memcached.
+  const std::string getValFromMemcachedVersionedKey(const std::string key);
+  
+  /// Return a list from memcached.
+  /// This function returns a vector with the elements still
+  /// serialized in it.
+  /// @param       listKey.
+  std::vector<std::string> getListFromMemcachedKey(const std::string& listKey) throw (DmException);
+
+  /// Add an item to a list on memcached.
+  /// This function takes a serialized object.
+  /// It uses memcached_append() to add the element,
+  /// so it does not have to download the list from
+  /// memcached.
+  /// @param listKey  The key of the list.
+  /// @param key      The key to add.  
+  void addToListFromMemcachedKey(const std::string& listKey, const std::string& key);
+
+  /// Remove an item from a list on memcached.
+  /// This function removes an element by adding it to
+  /// the list's blacklist. Like addToListFromMemcachedKey
+  /// it uses memcached_append.
+  /// @param listKey  The key of the list.
+  /// @param key      The key to remove.
+  void removeListItemFromMemcachedKey(const std::string& listKey, std::string& key);
+
+  /// Retrieve a list of values from a list of keys.
+  /// The function uses memcached_mget to get several
+  /// keys in one memcached bulk access.
+  /// If not all keys are found, it returns a empty list.
+  /// @param keyList  The list of keys to get.
+  /// @return         The list of serialized values.
+  std::vector<std::string> 
+         getValListFromMultipleMemcachedKeys(const std::vector<std::string>& keyList);
+
+  /// Store a list of FileReplicas on memcached.
+  /// @param replicas The list of replicas.
+  /// @param inode    The list key.
+  void setMemcachedFromReplicas(std::vector<FileReplica> replicas, ino_t inode);
+  
+  /// Store a key,value pair on memcached with a versioned key.
+  /// @param key   The versioned key as string.
+  /// @param value The value serialized as string. 
+  void setMemcachedFromVersionedKeyValue(const std::string key,
+                                         const std::string value);
 	/// Store a key,value pair on memcached.
 	/// @param key   The memcached key as string.
 	/// @param value The memcached value serialized as string.
 	void setMemcachedFromKeyValue(const std::string key,
 																						 const std::string value);
 
+  void delMemcachedFromVersionedKey(const std::string key);
 	/// Delete a key,value pair on memcached.
 	/// @param key   The memcached key as string.
 	void delMemcachedFromKey(const std::string key);
@@ -196,6 +303,7 @@ protected:
 private:
 };
 
+  
 };
 
 #endif	// MEMCACHED_H
