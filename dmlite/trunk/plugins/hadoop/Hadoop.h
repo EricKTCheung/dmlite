379e2ba71269c038eb17b3909b7f06de5542057e
abeche <abeche@4525493e-7705-40b1-a816-d608a930855b>
2012-04-20 11:19:57 +0000

    HadoopPlugins restructuration

--- dmlite/trunk/plugins/hadoop/Hadoop.h
+++ dmlite/trunk/plugins/hadoop/Hadoop.h
@@ -1,50 +1,28 @@
-/// @file    plugins/hadoop/Hadoop.h
-/// @brief   plugin to store data in a hadoop backend.
-/// @author  Alexandre Beche <abeche@cern.ch>
 #ifndef HADOOP_H
-#define	HADOOP_H
+#define HADOOP_H
 
-#include <vector>
-
-#include <dmlite/dm_io.h>
 #include <dmlite/dmlite++.h>
-#include <dmlite/common/Security.h>
 #include <dmlite/dummy/Dummy.h>
-#include <dmlite/common/Uris.h>
 
-#include "hdfs.h"
+#include "HadoopCatalog.h"
+#include "HadoopIO.h"
 
-namespace dmlite {
+namespace dmlite{
 
-class HadoopIOHandler{
+class HadoopFactory: public CatalogFactory{
 public:
+  HadoopFactory(CatalogFactory *catalogFactory) throw (DmException);
+  ~HadoopFactory() throw (DmException);
 
-  HadoopIOHandler(const std::string& uri, std::iostream::openmode openmode) throw (DmException);
-  ~HadoopIOHandler();
-
-  void close(void) throw (DmException);
-  size_t read(char* buffer, size_t count) throw (DmException);
-  size_t write(const char* buffer, size_t count) throw (DmException);
-  void seek(long offset, std::ios_base::seekdir whence) throw (DmException);
-  long tell(void) throw (DmException);
-  void flush(void) throw (DmException);
-  bool eof(void) throw (DmException);
-  void deleteFile (const char *filename) throw (DmException);
-
-private:
-  hdfsFS fs;		// Hadoop file system
-  hdfsFile file;	// Hadoop file descriptor
-  bool isEof;		// Set to true if end of the file is reached
-};
-
-class HadoopIOFactory{
-public:
-  HadoopIOFactory() throw (DmException);
   void configure(const std::string& key, const std::string& value) throw (DmException);
-  HadoopIOHandler *createIO(const std::string& uri, std::iostream::openmode openmode) throw (DmException);
+  Catalog* createCatalog() throw (DmException);
 protected:
+  CatalogFactory* nestedFactory;
 private:
+  std::string namenode;
+  std::string port;
+  std::string user;
+};
 };
 
-};
-#endif	// HADOOP_H
+#endif //HADOOP_H
