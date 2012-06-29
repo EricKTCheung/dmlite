3043dd54a323adf2c50a6c81ef02622ce9846fab
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-06-29 10:26:46 +0000

    fix string to sign for authenticated request.

--- plugins/s3/s3driver.cpp
+++ plugins/s3/s3driver.cpp
@@ -81,7 +81,7 @@ std::string S3Driver::canonicalize(std::string method, std::string bucket,
 
   if (headerMap.count("expires") > 0) {
     itHeaderMap = headerMap.find("expires");
-    stringToSign << itHeaderMap->first << ":" << itHeaderMap->second;
+    stringToSign << itHeaderMap->second;
   }
 
   // add bucket
