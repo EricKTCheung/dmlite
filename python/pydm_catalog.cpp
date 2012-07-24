3d2fff510e4d4f1b8d023dc8bd0bd2f5b04727a0
gjahn <gjahn@4525493e-7705-40b1-a816-d608a930855b>
2012-07-24 12:33:17 +0000

    Implemented purely virtual methods correctly.

--- python/pydm_catalog.cpp
+++ python/pydm_catalog.cpp
@@ -6,49 +6,41 @@
  * This file is included by pydmlite.cpp.
  */
 
-	class_<Catalog, boost::noncopyable>("Catalog", no_init)
-		// since this is a pure virtual class, the virtual functions
-		// may still have to be declared in the following format?		
-		//.def("f", boost::python::pure_virtual(&Base1::f));
-		// (with additional wrapper class)
+	class_<CatalogWrapper, bases< BaseInterface >, boost::noncopyable >("Catalog", no_init)
+		.def("setStackInstance", boost::python::pure_virtual(&Catalog::setStackInstance))
+		.def("setSecurityContext", boost::python::pure_virtual(&Catalog::setSecurityContext))
+		.def("changeDir", boost::python::pure_virtual(&Catalog::changeDir))
+		.def("getWorkingDir", boost::python::pure_virtual(&Catalog::getWorkingDir))
 
-		//.def("getImplId", &Catalog::getImplId) // function removed
-		.def("setStackInstance", &Catalog::setStackInstance)
-		.def("setSecurityContext", &Catalog::setSecurityContext)
-		.def("changeDir", &Catalog::changeDir)
-		.def("getWorkingDir", &Catalog::getWorkingDir)
-		//.def("getWorkingDirI", &Catalog::getWorkingDirI) // function removed
-		.def("extendedStat", &Catalog::extendedStat)
-		.def("addReplica", &Catalog::addReplica)
-		.def("deleteReplica", &Catalog::deleteReplica)
-		.def("getReplicas", &Catalog::getReplicas)
+		.def("extendedStat", boost::python::pure_virtual(&Catalog::extendedStat))
+		.def("addReplica", boost::python::pure_virtual(&Catalog::addReplica))
+		.def("deleteReplica", boost::python::pure_virtual(&Catalog::deleteReplica))
+		.def("getReplicas", boost::python::pure_virtual(&Catalog::getReplicas))
 
-		//.def("get", &Catalog::get)
-		//.def("put", static_cast< Location(Catalog::*)(const std::string&) > (&Catalog::put)) // function removed
-		//.def("put", static_cast< Location(Catalog::*)(const std::string&, const std::string&) > (&Catalog::put)) // function removed
-		//.def("putDone", &Catalog::putDone)
+		.def("symlink", boost::python::pure_virtual(&Catalog::symlink))
+		.def("unlink", boost::python::pure_virtual(&Catalog::unlink))
+		.def("create", boost::python::pure_virtual(&Catalog::create))
 
-		.def("symlink", &Catalog::symlink)
-		.def("unlink", &Catalog::unlink)
-		.def("create", &Catalog::create)
 		.def("umask", &Catalog::umask)
 		.def("setMode", &Catalog::setMode)
 		.def("setOwner", &Catalog::setOwner)
 		.def("setSize", &Catalog::setSize)
 		.def("setChecksum", &Catalog::setChecksum)
 		.def("setAcl", &Catalog::setAcl)
+
 		.def("utime", &Catalog::utime)
 		.def("getComment", &Catalog::getComment)
 		.def("setComment", &Catalog::setComment)
 		.def("setGuid", &Catalog::setGuid)
 
 		////////////////////////
-		// there are still some problems (about the void*-parameters)
-		// with the following bindings:
-		//.def("openDir", static_cast< void*(Catalog::*)(const std::string&) > (&Catalog::openDir))
-		//.def("closeDir", &Catalog::closeDir)
-		//.def("readDir", &Catalog::readDir)
-		//.def("readDirx", &Catalog::readDirx)
+		// there are severe problems with the void*-parameters / return values of the
+		// "Dir"-methods, thus, int-handles now represent directories instead of void*
+		// still not sure, if it works correctly -> commented out
+		//.def("openDirInt", &CatalogWrapper::openDirInt)
+		//.def("closeDirInt", &CatalogWrapper::closeDirInt)
+		//.def("readDirInt", &CatalogWrapper::readDirInt, return_internal_reference<>())
+		//.def("readDirxInt", &CatalogWrapper::readDirxInt, return_internal_reference<>())
 
 		.def("makeDir", &Catalog::makeDir)
 		.def("rename", &Catalog::rename)
@@ -58,8 +50,8 @@
 		.def("updateReplica", &Catalog::updateReplica)
 		;
 
-	class_<CatalogFactory, boost::noncopyable>("CatalogFactory", no_init)
-		.def("configure", &CatalogFactory::configure)
+
+	class_<CatalogFactoryWrapper, bases< BaseFactory >, boost::noncopyable>("CatalogFactory", no_init)
 		.def("createCatalog", static_cast< Catalog*(CatalogFactory::*)(PluginManager*) > (&CatalogFactory::createCatalog), return_internal_reference<>())
 		;
 	
