61057755cebebeaba3b5454a93fbb7a2f847949d
aalvarez <aalvarez@4525493e-7705-40b1-a816-d608a930855b>
2012-08-17 12:41:25 +0000

    LCGDM-662: Added requested methods

--- dmlite/trunk/src/core/builtin/Authn.h
+++ dmlite/trunk/src/core/builtin/Authn.h
@@ -22,14 +22,22 @@ namespace dmlite {
     GroupInfo newGroup   (const std::string& gname) throw (DmException);
     GroupInfo getGroup   (gid_t gid) throw (DmException);
     GroupInfo getGroup   (const std::string& groupName) throw (DmException);
+    GroupInfo getGroup   (const std::string& key,
+                          const boost::any& value) throw (DmException);
     void      updateGroup(const GroupInfo& group) throw (DmException);
     void      deleteGroup(const std::string& groupName) throw (DmException);
 
     UserInfo newUser   (const std::string& uname)    throw (DmException);
     UserInfo getUser   (const std::string& userName) throw (DmException);
-    UserInfo getUser   (const std::string& userName, gid_t* group) throw (DmException);
+    UserInfo getUser   (const std::string& userName,
+                        gid_t* group) throw (DmException);
+    UserInfo getUser   (const std::string& key,
+                        const boost::any& value) throw (DmException);
     void     updateUser(const UserInfo& user) throw (DmException);
     void     deleteUser(const std::string& userName) throw (DmException);
+    
+    std::vector<GroupInfo> getGroups(void) throw (DmException);
+    std::vector<UserInfo>  getUsers (void) throw (DmException);
 
     void getIdMap(const std::string& userName,
                   const std::vector<std::string>& groupNames,
