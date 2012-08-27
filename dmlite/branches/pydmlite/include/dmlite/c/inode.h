1b75c00c075c801a38bdaa0c35b099d1a00dd67f
rocha <rocha@4525493e-7705-40b1-a816-d608a930855b>
2012-08-27 11:32:30 +0000

    last merge from trunk

--- dmlite/branches/pydmlite/include/dmlite/c/inode.h
+++ dmlite/branches/pydmlite/include/dmlite/c/inode.h
@@ -346,7 +346,10 @@ int dmlite_iclosedir(dmlite_context* context, dmlite_idir* dir);
  * @brief         Reads a directory. Extended data.
  * @param context The DM context.
  * @param dir     The directory to read, as returned by dmlite_opendir.
- * @return        NULL on failure, or end of dir.
+ * @return        A pointer to a struct with the recovered data.
+ *                NULL on failure, or end of dir. If an error occurred,
+ *                dm_errno(context) will be different than 0.
+ * @note          The pointer is internally allocated. Do not free it.
  */
 dmlite_xstat* dmlite_ireaddirx(dmlite_context* context, dmlite_idir* dir);
 
@@ -354,7 +357,10 @@ dmlite_xstat* dmlite_ireaddirx(dmlite_context* context, dmlite_idir* dir);
  * @brief         Reads a directory.
  * @param context The DM context.
  * @param dir     The directory to read, as returned by dmlite_opendir.
- * @return        NULL on failure, or end of dir.
+ * @return        A pointer to a struct with the recovered data.
+ *                NULL on failure, or end of dir. If an error occurred,
+ *                dm_errno(context) will be different than 0.
+ * @note          The pointer is internally allocated. Do not free it.
  */
 struct dirent* dmlite_ireaddir(dmlite_context* context, dmlite_idir* dir);
 
