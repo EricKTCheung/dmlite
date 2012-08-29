a7a7b1d8261db73eb31bc2aa2fb2c3cf4a67899c
gjahn <gjahn@4525493e-7705-40b1-a816-d608a930855b>
2012-08-29 17:38:35 +0000

    Split pydmlite compilation to reduce compilation time.

--- dmlite/trunk/python/poolmanager.cpp
+++ dmlite/trunk/python/poolmanager.cpp
@@ -3,24 +3,28 @@
  *
  * Python bindings for poolmanager.h from the c++ dmlite library
  * via Boost:Python.
- * This file is included by pydmlite.cpp.
  */
-	
-	class_<Pool, bases< Extensible > >("Pool", init<>())
-		.def_readwrite("name", &Pool::name)
-		.def_readwrite("type", &Pool::type)
-		;
 
-	class_< std::vector< Pool > >("vector_Pool")
-		.def(vector_indexing_suite< std::vector< Pool > >()) // only works with operator== and != in Pool
-		;
-	
-	class_<PoolManagerWrapper, bases< BaseInterface >, boost::noncopyable>("PoolManager", no_init)
-		.def("getPools", boost::python::pure_virtual(&PoolManager::getPools))
-		.def("getPool", boost::python::pure_virtual(&PoolManager::getPool))
-		.def("whereToRead", boost::python::pure_virtual( static_cast< Location(PoolManager::*)(const std::string&) >  (&PoolManager::whereToRead) ))
-		.def("whereToWrite", boost::python::pure_virtual(&PoolManager::whereToWrite))
-		;
+#include "pydmlite.h"
+#include "poolmanagerwrapper.cpp"
+
+void export_poolmanager()
+{
+    class_<Pool, bases< Extensible > >("Pool", init<>())
+        .def_readwrite("name", &Pool::name)
+        .def_readwrite("type", &Pool::type)
+        ;
+
+    class_< std::vector< Pool > >("vector_Pool")
+        .def(vector_indexing_suite< std::vector< Pool > >()) // only works with operator== and != in Pool
+        ;
+    
+    class_<PoolManagerWrapper, bases< BaseInterface >, boost::noncopyable>("PoolManager", no_init)
+        .def("getPools", boost::python::pure_virtual(&PoolManager::getPools))
+        .def("getPool", boost::python::pure_virtual(&PoolManager::getPool))
+        .def("whereToRead", boost::python::pure_virtual( static_cast< Location(PoolManager::*)(const std::string&) >  (&PoolManager::whereToRead) ))
+        .def("whereToWrite", boost::python::pure_virtual(&PoolManager::whereToWrite))
+        ;
 
     enum_<PoolManager::PoolAvailability>("PoolAvailability")
         .value("kAny", PoolManager::kAny)
@@ -30,6 +34,7 @@
         .value("kForBoth", PoolManager::kForBoth)
         ;
 
-	class_<PoolManagerFactoryWrapper, bases< BaseFactory >, boost::noncopyable>("PoolManagerFactory", no_init)
-		.def("createPoolManager", boost::python::pure_virtual(&PoolManagerFactoryWrapper::createPoolManager), return_value_policy<manage_new_object>())
-		;
+    class_<PoolManagerFactoryWrapper, bases< BaseFactory >, boost::noncopyable>("PoolManagerFactory", no_init)
+        .def("createPoolManager", boost::python::pure_virtual(&PoolManagerFactoryWrapper::createPoolManager), return_value_policy<manage_new_object>())
+        ;
+}
--- dmlite/trunk/python/pydmlite.cpp
+++ dmlite/trunk/python/pydmlite.cpp
@@ -1,101 +1,87 @@
 /*
  * pydmlite.cpp
  *
- * Python bindings for the c++ dmlite library via Boost:Python.
+ * Python bindings for the c++ dmlite library via Boost:Python,
+ * especially the bindings for dmlite.h are directly here.
  * The resulting python module pydmlite gives access to most
  * the available functionality from dmlite.
  */
 
-#include <boost/python.hpp>
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include "pydmlite.h"
 
-#include "authn.h"
-#include "base.h"
-#include "catalog.h"
-#include "dmlite.h"
-#include "exceptions.h"
-#include "inode.h"
-#include "io.h"
-#include "pooldriver.h"
-#include "poolmanager.h"
-#include "utils/urls.h"
+void export_extensible();
+void export_errno();
+void export_types();
 
-using namespace dmlite;
-using namespace boost::python;
-
-
-#include "helpers.cpp"
-// include wrapper classes
-#include "authnwrapper.cpp"
-#include "basewrapper.cpp"
-#include "catalogwrapper.cpp"
-#include "inodewrapper.cpp"
-#include "iowrapper.cpp"
-#include "pooldriverwrapper.cpp"
-#include "poolmanagerwrapper.cpp"
+void export_authn();
+void export_base();
+void export_catalog();
+void export_exceptions();
+void export_inode();
+void export_io();
+void export_pooldriver();
+void export_poolmanager();
 
 
 BOOST_PYTHON_MODULE(pydmlite)
 {
-	// These python bindings are compliant with version 20120817.
-	scope().attr("API_VERSION") = API_VERSION; 
-	
-	class_<PluginManager, boost::noncopyable>("PluginManager")
-		.def("loadPlugin", &PluginManager::loadPlugin)
-		.def("configure", &PluginManager::configure)
-		.def("loadConfiguration", &PluginManager::loadConfiguration)
+    // These python bindings are compliant with version 20120817.
+    scope().attr("API_VERSION") = API_VERSION; 
+    
+    class_<PluginManager, boost::noncopyable>("PluginManager")
+        .def("loadPlugin", &PluginManager::loadPlugin)
+        .def("configure", &PluginManager::configure)
+        .def("loadConfiguration", &PluginManager::loadConfiguration)
 
-		.def("registerAuthnFactory", &PluginManager::registerAuthnFactory)
-		.def("registerINodeFactory", &PluginManager::registerINodeFactory)
-		.def("registerCatalogFactory", &PluginManager::registerCatalogFactory)
-		.def("registerPoolManagerFactory", &PluginManager::registerPoolManagerFactory)
-		.def("registerIOFactory", &PluginManager::registerIOFactory)
-		.def("registerPoolDriverFactory", &PluginManager::registerPoolDriverFactory)
-		
-		.def("getAuthnFactory", &PluginManager::getAuthnFactory, return_value_policy<reference_existing_object>())
-		.def("getINodeFactory", &PluginManager::getINodeFactory, return_value_policy<reference_existing_object>())
-		.def("getCatalogFactory", &PluginManager::getCatalogFactory, return_value_policy<reference_existing_object>())
-		.def("getPoolManagerFactory", &PluginManager::getPoolManagerFactory, return_value_policy<reference_existing_object>())
-		.def("getPoolDriverFactory", &PluginManager::getPoolDriverFactory, return_value_policy<reference_existing_object>())
-		.def("getIOFactory", &PluginManager::getIOFactory, return_value_policy<reference_existing_object>())
-		;
-	
-	class_<StackInstance>("StackInstance", init<PluginManager*>())
-		.def("set", &StackInstance::set)
-		.def("get", &StackInstance::get)
-		.def("erase", &StackInstance::erase)
-		.def("getPluginManager", &StackInstance::getPluginManager, return_value_policy<reference_existing_object>())
-		.def("setSecurityCredentials", &StackInstance::setSecurityCredentials)
-		.def("setSecurityContext", &StackInstance::setSecurityContext)
-		.def("getSecurityContext", &StackInstance::getSecurityContext, return_value_policy<reference_existing_object>())
-		.def("getAuthn", &StackInstance::getAuthn, return_value_policy<reference_existing_object>())
-		.def("getINode", &StackInstance::getINode, return_value_policy<reference_existing_object>())
-		.def("getCatalog", &StackInstance::getCatalog, return_value_policy<reference_existing_object>())
-		.def("isTherePoolManager", &StackInstance::isTherePoolManager)
-		.def("getPoolManager", &StackInstance::getPoolManager, return_value_policy<reference_existing_object>())
-		.def("getPoolDriver", &StackInstance::getPoolDriver, return_value_policy<reference_existing_object>())
-		.def("getIODriver", &StackInstance::getIODriver, return_value_policy<reference_existing_object>())
-		;
+        .def("registerAuthnFactory", &PluginManager::registerAuthnFactory)
+        .def("registerINodeFactory", &PluginManager::registerINodeFactory)
+        .def("registerCatalogFactory", &PluginManager::registerCatalogFactory)
+        .def("registerPoolManagerFactory", &PluginManager::registerPoolManagerFactory)
+        .def("registerIOFactory", &PluginManager::registerIOFactory)
+        .def("registerPoolDriverFactory", &PluginManager::registerPoolDriverFactory)
+        
+        .def("getAuthnFactory", &PluginManager::getAuthnFactory, return_value_policy<reference_existing_object>())
+        .def("getINodeFactory", &PluginManager::getINodeFactory, return_value_policy<reference_existing_object>())
+        .def("getCatalogFactory", &PluginManager::getCatalogFactory, return_value_policy<reference_existing_object>())
+        .def("getPoolManagerFactory", &PluginManager::getPoolManagerFactory, return_value_policy<reference_existing_object>())
+        .def("getPoolDriverFactory", &PluginManager::getPoolDriverFactory, return_value_policy<reference_existing_object>())
+        .def("getIOFactory", &PluginManager::getIOFactory, return_value_policy<reference_existing_object>())
+        ;
+    
+    class_<StackInstance>("StackInstance", init<PluginManager*>())
+        .def("set", &StackInstance::set)
+        .def("get", &StackInstance::get)
+        .def("erase", &StackInstance::erase)
+        .def("getPluginManager", &StackInstance::getPluginManager, return_value_policy<reference_existing_object>())
+        .def("setSecurityCredentials", &StackInstance::setSecurityCredentials)
+        .def("setSecurityContext", &StackInstance::setSecurityContext)
+        .def("getSecurityContext", &StackInstance::getSecurityContext, return_value_policy<reference_existing_object>())
+        .def("getAuthn", &StackInstance::getAuthn, return_value_policy<reference_existing_object>())
+        .def("getINode", &StackInstance::getINode, return_value_policy<reference_existing_object>())
+        .def("getCatalog", &StackInstance::getCatalog, return_value_policy<reference_existing_object>())
+        .def("isTherePoolManager", &StackInstance::isTherePoolManager)
+        .def("getPoolManager", &StackInstance::getPoolManager, return_value_policy<reference_existing_object>())
+        .def("getPoolDriver", &StackInstance::getPoolDriver, return_value_policy<reference_existing_object>())
+        .def("getIODriver", &StackInstance::getIODriver, return_value_policy<reference_existing_object>())
+        ;
 
-	class_<PluginIdCard>("PluginIdCard")
-		.def_readonly("ApiVersion", &PluginIdCard::ApiVersion)
-		// registerPlugin not exposed yet
-		;
+    class_<PluginIdCard>("PluginIdCard")
+        .def_readonly("ApiVersion", &PluginIdCard::ApiVersion)
+        // registerPlugin not exposed yet
+        ;
 
-	scope().attr("PLUGIN_ID_HEADER") = PLUGIN_ID_HEADER; 
+    scope().attr("PLUGIN_ID_HEADER") = PLUGIN_ID_HEADER; 
 
+    export_extensible();
+    export_errno();
+    export_types();
 
-#include "extensible.cpp"
-#include "errno.cpp"
-#include "types.cpp"
-
-
-#include "authn.cpp"
-#include "base.cpp"
-#include "catalog.cpp"
-#include "exceptions.cpp"
-#include "inode.cpp"
-#include "io.cpp"
-#include "pooldriver.cpp"
-#include "poolmanager.cpp"
+    export_authn();
+    export_base();
+    export_catalog();
+    export_exceptions();
+    export_inode();
+    export_io();
+    export_pooldriver();
+    export_poolmanager();
 }
