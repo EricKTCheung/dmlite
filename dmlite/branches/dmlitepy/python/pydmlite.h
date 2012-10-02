bed9e59f65ede70c37fb73ae3544b20479cd21de
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-10-02 17:37:58 +0000

    branching trunk to dmlitepy at HEAD_REVISION 7295 for developing python access from c++

---
+++ dmlite/branches/dmlitepy/python/pydmlite.h
@@ -0,0 +1,22 @@
+/*
+ * pydmlite.h
+ *
+ * Includes required by all the pydmlite module files.
+ */
+
+#include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+
+#include "authn.h"
+#include "base.h"
+#include "catalog.h"
+#include "dmlite.h"
+#include "exceptions.h"
+#include "inode.h"
+#include "io.h"
+#include "pooldriver.h"
+#include "poolmanager.h"
+#include "utils/urls.h"
+
+using namespace dmlite;
+using namespace boost::python;
---
+++ dmlite/branches/dmlitepy/tests/c/test-manager.c
@@ -0,0 +1,71 @@
+#include <dmlite/c/catalog.h>
+#include <dmlite/c/dmlite.h>
+#include <dmlite/common/errno.h>
+#include "utils.h"
+
+
+int main(int argn, char** argv)
+{
+  dmlite_manager* manager;
+  dmlite_context* context;
+  
+  printf("DMLite API Version: %d\n", dmlite_api_version());
+  
+  /* Creation */
+  SECTION("Creation");
+  manager = dmlite_manager_new();
+  TEST_ASSERT(manager != NULL);
+  
+  SECTION("Loading errors")
+  TEST_ASSERT_EQUAL(DMLITE_CFGERR(ENOENT),
+                    dmlite_manager_load_plugin(manager, "/does/not/exist", "plugin_something"));
+  TEST_ASSERT_EQUAL(DMLITE_CFGERR(DMLITE_NO_SUCH_SYMBOL),
+                    dmlite_manager_load_plugin(manager, "./plugin_mock.so", "plugin_fake"));
+  
+  /* Load the mock plug-in */  
+  TEST_MANAGER_CALL(manager, dmlite_manager_load_plugin, "./plugin_mock.so", "plugin_mock");
+  
+  /* Set an unknown option */
+  TEST_ASSERT_EQUAL(DMLITE_CFGERR(DMLITE_UNKNOWN_KEY),
+                    dmlite_manager_set(manager, "Option", "Value"));
+  
+  /* Instantiate */
+  context = dmlite_context_new(manager);
+  TEST_ASSERT(context != NULL);
+  if (context == NULL) {
+    log_failure("%s", dmlite_manager_error(manager));
+    exit(1);
+  }
+  
+  /* Doing anything must fail because the credentials weren't set */
+  struct stat s;
+  TEST_ASSERT_EQUAL(DMLITE_SYSERR(DMLITE_NO_SECURITY_CONTEXT),
+                    dmlite_stat(context, "/", &s));
+  
+  /* Set credentials */
+  SECTION("Security context");
+  dmlite_credentials creds;
+  memset(&creds, 0, sizeof(creds));
+  creds.client_name = "root";
+  TEST_CONTEXT_CALL(context, dmlite_setcredentials, &creds);
+  
+  /* Get context back and check */  
+  const dmlite_security_context* secCtx = dmlite_get_security_context(context);
+  TEST_ASSERT(secCtx != NULL);
+  
+  TEST_ASSERT_EQUAL(1, secCtx->ngroups);
+  TEST_ASSERT_STR_EQUAL("root", secCtx->user.name);
+  
+  dmlite_any* any = dmlite_any_dict_get(secCtx->user.extra, "uid");
+  TEST_ASSERT_EQUAL(0, dmlite_any_to_long(any));
+  dmlite_any_free(any);
+  
+  TEST_ASSERT_STR_EQUAL(creds.client_name, secCtx->credentials->client_name);
+  TEST_ASSERT_EQUAL(creds.nfqans, secCtx->credentials->nfqans)
+
+  /* Clean-up */
+  dmlite_context_free(context);
+  dmlite_manager_free(manager);
+  
+  return TEST_FAILURES;
+}
