15c9c3d2cac319f002a4cd46878b8a018ab0cb1f
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-10-12 08:15:12 +0000

    add raisable DmException

--- python/pydmlite.cpp
+++ python/pydmlite.cpp
@@ -24,13 +24,33 @@ void export_poolmanager();
 
 ExtendedStat::FileStatus identity_(ExtendedStat::FileStatus x)
 { 
-  return x;
+    return x;
 }
+
+// taken from http://stackoverflow.com/questions/9620268/boost-python-custom-exception-class
+PyObject* createExceptionClass(const char* name, PyObject* baseTypeObj = PyExc_Exception)
+{
+    using std::string;
+    namespace bp = boost::python;
+
+    string scopeName = bp::extract<string>(bp::scope().attr("__name__"));
+    string qualifiedName0 = scopeName + "." + name;
+    char* qualifiedName1 = const_cast<char*>(qualifiedName0.c_str());
+
+    PyObject* typeObj = PyErr_NewException(qualifiedName1, baseTypeObj, 0);
+    if(!typeObj) bp::throw_error_already_set();
+    bp::scope().attr(name) = bp::handle<>(bp::borrowed(typeObj));
+    return typeObj;
+}
+
+PyObject* dmExceptionTypeObj = 0;
 
 BOOST_PYTHON_MODULE(pydmlite)
 {
     // These python bindings are compliant with version 20120817.
     scope().attr("API_VERSION") = API_VERSION; 
+
+    dmExceptionTypeObj = createExceptionClass("DmException");
 
     def("identity", identity_);
     
