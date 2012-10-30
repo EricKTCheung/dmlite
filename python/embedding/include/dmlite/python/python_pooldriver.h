df95826bbbb18d580db30837edfb19b0d7e95e49
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-10-30 10:42:04 +0000

    added PoolDriver and PoolHandler embedding

---
+++ python/embedding/include/dmlite/python/python_pooldriver.h
@@ -0,0 +1,114 @@
+/// @file   python/embedding/include/dmlite/python/python_pooldriver.h
+/// @brief  Python PythonPoolDriver API.
+/// @author Martin Philipp Hellmich <martin.hellmich@cern.ch>
+#ifndef DMLITE_CPP_PYTHONPOOLDRIVER_H
+#define DMLITE_CPP_PYTHONPOOLDRIVER_H
+
+#include <boost/python.hpp>
+
+#include <dmlite/cpp/dmlite.h>
+#include <dmlite/cpp/pooldriver.h>
+#include <dmlite/cpp/poolmanager.h>
+#include <dmlite/python/python_common.h>
+
+namespace dmlite {
+  
+  // Forward declarations.
+  //class Pool;
+  //class StackInstance;
+  
+
+  /// Handler for a pool. Works similary to a file handler.
+  class PythonPoolHandler : public virtual PoolHandler {
+   public:
+    PythonPoolHandler(boost::python::object module_obj);
+    /// Destructor
+    ~PythonPoolHandler();
+
+    /// Get the pool type of this pool.
+    std::string getPoolType(void) throw (DmException);
+
+    /// Get the pool name of this pool.
+    std::string getPoolName(void) throw (DmException);
+
+    /// Get the total space of this pool.
+    uint64_t getTotalSpace(void) throw (DmException);
+
+    /// Get the free space of this pool.
+    uint64_t getFreeSpace(void) throw (DmException);
+
+    /// Check if the pool is actually available
+    bool poolIsAvailable(bool write = true) throw (DmException);
+    
+    /// Check if a replica is available
+    bool replicaIsAvailable(const Replica& replica) throw (DmException);
+
+    /// Get the actual location of the file replica. This is pool-specific.
+    Location whereToRead(const Replica& replica) throw (DmException);
+
+    /// Remove a replica from the pool.
+    void removeReplica(const Replica& replica) throw (DmException);
+
+    /// Get where to put a file
+    Location whereToWrite(const std::string& path) throw (DmException);
+   private:
+    PythonMain py;
+  };
+
+  /// Interface for a pool driver
+  class PythonPoolDriver: public virtual PoolDriver {
+   public:
+    PythonPoolDriver(boost::python::object module_obj);
+    /// Destructor
+    ~PythonPoolDriver();
+
+    void setStackInstance(StackInstance* si) throw (DmException);
+    void setSecurityContext(const SecurityContext* ctx) throw (DmException);
+    /// String ID of the implementation.
+    std::string getImplId(void) const throw();
+
+    /// Create a handler.
+    PythonPoolHandler* createPoolHandler(const std::string& poolName) throw (DmException);
+    
+    /// Called just before adding the pool to the database.
+    /// To be used by a plugin, in case it needs to do some previous preparations.
+    /// (i.e. legacy filesystem will actually create the pool here)
+    void toBeCreated(const Pool& pool) throw (DmException);
+    
+    /// Called just after a pool is added to the database.
+    void justCreated(const Pool& pool) throw (DmException);
+    
+    /// Called when updating a pool.
+    void update(const Pool& pool) throw (DmException);
+    
+    /// Called just before a pool of this type is removed.
+    /// @note The driver may remove the pool itself (i.e. filesystem)
+    void toBeDeleted(const Pool& pool) throw (DmException);
+   private:
+    PythonMain py;
+  };
+
+  /// PythonPoolDriver factory
+  class PythonPoolDriverFactory: public virtual PoolDriverFactory {
+   public:
+    PythonPoolDriverFactory(std::string pymodule);
+    /// Destructor.
+    ~PythonPoolDriverFactory();
+
+    /// Supported pool type
+    std::string implementedPool() throw ();
+
+    void configure(const std::string& key, const std::string& value) throw (DmException);
+
+   protected:
+    friend class StackInstance;
+
+    /// Instantiate the implemented pool driver.
+    PythonPoolDriver* createPoolDriver(void) throw (DmException);
+   private:
+    PythonMain py;
+  };
+
+};
+
+#endif	// DMLITE_CPP_PYTHONPOOLDRIVER_H
---
+++ python/embedding/src/PythonPoolDriver.cpp
@@ -0,0 +1,270 @@
+/// @file   python/embedding/src/PythonPoolDriver.cpp
+/// @brief  Python PythonPoolDriver API.
+/// @author Martin Philipp Hellmich <martin.hellmich@cern.ch>
+
+#include <dmlite/python/python_pooldriver.h>
+
+using namespace dmlite;
+using namespace boost::python;
+
+
+PythonPoolHandler::PythonPoolHandler(boost::python::object module_obj)
+{
+  this->py["module"] = module_obj;
+}
+
+PythonPoolHandler::~PythonPoolHandler()
+{
+}
+
+
+std::string PythonPoolHandler::getPoolType(void) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("getPoolType")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::getPoolType");
+    }
+    return extract<std::string>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+std::string PythonPoolHandler::getPoolName(void) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("getPoolName")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::getPoolName");
+    }
+    return extract<std::string>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+uint64_t PythonPoolHandler::getTotalSpace(void) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("getTotalSpace")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: Yo shouldn't return None here", "PythonPoolHandler::getTotalSpace");
+    }
+    return extract<uint64_t>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+uint64_t PythonPoolHandler::getFreeSpace(void) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("getFreeSpace")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::getFreeSpace");
+    }
+    return extract<uint64_t>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+bool PythonPoolHandler::poolIsAvailable(bool) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("poolIsAvailable")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::poolIsAvailable");
+    }
+    return extract<bool>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+bool PythonPoolHandler::replicaIsAvailable(const Replica& replica) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("replicaIsAvailable")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::replicaIsAvailable");
+    }
+    return extract<bool>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+Location PythonPoolHandler::whereToRead(const Replica& replica) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("whereToRead")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::whereToRead");
+    }
+    return extract<Location>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+void PythonPoolHandler::removeReplica(const Replica& replica) throw (DmException)
+{
+  CALL_PYTHON(removeReplica, replica);
+}
+
+
+Location PythonPoolHandler::whereToWrite(const std::string& path) throw (DmException)
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("whereToWrite")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolHandler::whereToWrite");
+    }
+    return extract<Location>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+PythonPoolDriver::PythonPoolDriver(boost::python::object module_obj)
+{
+  this->py["module"] = module_obj;
+}
+
+PythonPoolDriver::~PythonPoolDriver()
+{
+}
+
+std::string PythonPoolDriver::getImplId(void) const throw()
+{
+  return std::string("PythonPoolDriver");
+}
+
+
+
+void PythonPoolDriver::setStackInstance(StackInstance* si) throw (DmException)
+{
+  CALL_PYTHON(setStackInstance, ptr(si));
+}
+
+
+
+void PythonPoolDriver::setSecurityContext(const SecurityContext* ctx) throw (DmException)
+{
+  CALL_PYTHON(setSecurityContext, ptr(ctx));
+}
+
+
+
+PythonPoolHandler* PythonPoolDriver::createPoolHandler(const std::string& poolName) throw (DmException)
+{
+  object module;
+  try {
+    object pymoduleFac = boost::any_cast<object>(this->py["pymoduleFac"]);
+    module = pymoduleFac.attr("createPoolHandler")();
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+
+  return new PythonPoolHandler(module);
+}
+
+
+void PythonPoolDriver::toBeCreated(const Pool& pool) throw (DmException)
+{
+  CALL_PYTHON(toBeCreated, pool);
+}
+
+
+void PythonPoolDriver::justCreated(const Pool& pool) throw (DmException)
+{
+  CALL_PYTHON(justCreated, pool);
+}
+
+
+void PythonPoolDriver::update(const Pool& pool) throw (DmException)
+{
+  CALL_PYTHON(update, pool);
+}
+
+
+void PythonPoolDriver::toBeDeleted(const Pool& pool) throw (DmException)
+{
+  CALL_PYTHON(toBeDeleted, pool);
+}
+
+PythonPoolDriverFactory::PythonPoolDriverFactory(std::string pymodule)
+{
+  Py_Initialize();
+
+  object moduleFac;
+  object pymoduleFac;
+  try {
+    moduleFac = import(pymodule.c_str());
+    pymoduleFac = moduleFac.attr("pyPoolDriverFactory")();
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+ 
+  this->py["pymoduleFac"] = pymoduleFac;
+}
+
+PythonPoolDriverFactory::~PythonPoolDriverFactory()
+{
+}
+
+
+void PythonPoolDriverFactory::configure(const std::string& key, const std::string& value) throw(DmException)
+{
+  try {
+    object pymoduleFac = boost::any_cast<object>(this->py["pymoduleFac"]);
+    pymoduleFac.attr("configure")(key, value);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+std::string PythonPoolDriverFactory::implementedPool() throw ()
+{
+  try {
+    object mod = boost::any_cast<object>(this->py["module"]);
+    object result = mod.attr("implementedPool")();
+    if (result.ptr() == Py_None) {
+      throw DmException(DMLITE_SYSERR(DMLITE_UNEXPECTED_EXCEPTION), "%s: You shouldn't return None here", "PythonPoolDriverFactory::implementedPool");
+    }
+    return extract<std::string>(result);
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+}
+
+
+PythonPoolDriver* PythonPoolDriverFactory::createPoolDriver(void) throw (DmException)
+{
+  object module;
+  try {
+    object pymoduleFac = boost::any_cast<object>(this->py["pymoduleFac"]);
+    module = pymoduleFac.attr("createPoolDriver")();
+  } catch (error_already_set const &) {
+    PyErr_Print();
+  }
+
+  return new PythonPoolDriver(module);
+}
