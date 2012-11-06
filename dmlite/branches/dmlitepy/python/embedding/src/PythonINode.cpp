464bc58a3947b042372711c2470fee6f8308c490
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-11-06 18:31:55 +0000

    added c++ exception handling test

--- dmlite/branches/dmlitepy/python/embedding/src/PythonINode.cpp
+++ dmlite/branches/dmlitepy/python/embedding/src/PythonINode.cpp
@@ -134,7 +134,27 @@ ExtendedStat PythonINode::extendedStat(ino_t inode) throw (DmException)
     }
     meta = extract<ExtendedStat>(result);
   } catch (error_already_set const &) {
-    PyErr_Print();
+  std::string excString;
+
+  PyObject *exc,*val,*tb;
+  PyErr_Fetch(&exc,&val,&tb);
+  PyErr_NormalizeException(&exc,&val,&tb);
+  handle<> hexc(exc),hval(allow_null(val)),htb(allow_null(tb));
+
+  if(!hval) {
+    excString = extract<std::string>(str(hexc));
+  }
+    else {
+    object traceback(import("traceback"));
+    object format_exception(traceback.attr("format_exception"));
+    object formatted_list(format_exception(hexc,hval,htb));
+    object formatted(str("").join(formatted_list));
+    excString = extract<std::string>(formatted);
+  }
+
+  throw DmException(DMLITE_SYSERR(DMLITE_UNKNOWN_ERROR),
+                    excString);
+  }
   }
 
   return meta;
