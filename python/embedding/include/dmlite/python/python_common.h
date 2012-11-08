fe8d3ed0262d8e295d688a8e695a93da011bb275
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2012-11-08 13:52:49 +0000

    corrected spec again

--- python/embedding/include/dmlite/python/python_common.h
+++ python/embedding/include/dmlite/python/python_common.h
@@ -35,26 +35,22 @@ public:
 /*
 class PythonCommon {
 public:
-template <class T>
-inline T runPythonMethod(PythonMain py, const char *methodName, DmException e, ...)
-{
-  using namespace boost::python;
-
-  T pythonResult;
-
-  try {
-    object mod = boost::any_cast<object>(py["module"]);
-    object result = mod.attr(methodName)(va_arg);
-    if (result.ptr() == Py_None) {
-      throw e;
-    }
-    pythonResult = extract<T>(result);
-  } catch (error_already_set const &) {
-    PyErr_Print();
-  }
+  template <class T>
+  T runPythonMethod(PythonMain py, const char *methodName, DmException e, ...)
+  {
+    using namespace boost::python;
   
-  return pythonResult;
-};
+    try {
+      object mod = boost::any_cast<object>(py["module"]);
+      object result = mod.attr(methodName)(va_arg);
+      if (result.ptr() == Py_None) {
+        throw e;
+      }
+      return extract<T>(result);
+    } catch (error_already_set const &) {
+      PyErr_Print();
+    }
+  };
 };
 */
 };
