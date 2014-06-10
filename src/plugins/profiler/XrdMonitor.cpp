99d9523ee22c308b9a186187ab96d88f8d89b7c5
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2014-06-10 13:26:48 +0000

    correct short user ident to omit the newline at the end

--- src/plugins/profiler/XrdMonitor.cpp
+++ src/plugins/profiler/XrdMonitor.cpp
@@ -262,7 +262,7 @@ int XrdMonitor::sendShortUserIdent(const kXR_char dictid)
   int ret = 0;
 
   char info[1024+256];
-  snprintf(info, 1024+256, "%s.%d:%lld@%s\n",
+  snprintf(info, 1024+256, "%s.%d:%lld@%s",
            username_.c_str(), pid_, sid_, hostname_.c_str());
 
   syslog(LOG_MAKEPRI(LOG_USER, LOG_DEBUG), "%s:\n%s",
