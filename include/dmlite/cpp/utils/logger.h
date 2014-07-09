2e5f3f112bb70b73cdf59769563585c206259a57
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2014-07-09 12:58:07 +0000

    Added new logger class

---
+++ include/dmlite/cpp/utils/logger.h
@@ -0,0 +1,149 @@
+#ifndef Logger_HH
+#define Logger_HH
+
+#include <syslog.h>
+
+#include <sstream>
+#include <string>
+
+#include <map>
+#include <vector>
+
+
+#define Log(lvl, mymask, where, what) 												\
+do{                                											\
+	if (Logger::get()->getLevel() >= lvl && Logger::get()->isLogged(mymask)) 	\
+	{    																	\
+		std::ostringstream outs;                                   			\
+		outs << where << "::" << __func__ << " : " << what;                      			\
+		Logger::get()->log((Logger::Level)lvl, outs.str());    				\
+	}                                                             			\
+}while(0)                                                               			\
+
+#define Err(lvl, mymask, where, what) 												\
+do{                                											\
+	if (Logger::get()->getLevel() >= lvl && Logger::get()->isLogged(mymask)) 	\
+	{    																	\
+		std::ostringstream outs;                                   			\
+		outs << "ERROR " << where << "::" << __func__ << " : " << what;                      			\
+		Logger::get()->log((Logger::Level)lvl, outs.str());    				\
+	}                                                             			\
+}while(0)       
+
+/**
+ * A Logger class
+ */
+class Logger
+{
+
+public:
+	/// typedef for a bitmask (long long)
+	typedef unsigned long long bitmask;
+	/// typedef for a component name (std:string)
+	typedef std::string component;
+
+	static bitmask unregistered;
+
+    /**
+     * Use the same values for log levels as syslog
+     */
+    enum Level
+    {
+        BASE,
+        WARNING,
+        NOTICE,
+        INFO,
+        DEBUG
+    };
+
+    /// Destructor
+    ~Logger();
+
+    static Logger *instance;
+    
+    /// @return the singleton instance
+    static Logger *get()
+    {
+    	if (instance == 0) instance = new Logger();
+    	return instance;
+    }
+
+    /// @return the current debug level
+    short getLevel() const
+    {
+        return level;
+    }
+
+    /// @param lvl : the logging level that will be set
+    void setLevel(Level lvl)
+    {
+        level = lvl;
+    }
+
+    /// @return true if the given component is being logged, false otherwise
+    bool isLogged(bitmask m) const
+    {
+        if (mask == 0) return mask & unregistered;
+    	return mask & m;
+    }
+    
+    /// @param comp : the component that will be registered for logging
+    void registerComponent(component const &  comp);
+    
+    /// @param components : list of components that will be registered for logging
+    void registerComponents(std::vector<component> const & components);
+
+    /// Sets if a component has to be logged or not
+    /// @param comp : the component name
+    /// @param tobelogged : true if we want to log this component
+    void setLogged(component const &comp, bool tobelogged);
+    
+    /**
+     * Logs the message
+     *
+     * @param lvl : log level of the message
+     * @param component : bitmask assignet to the given component
+     * @param msg : the message to be logged
+     */
+    void log(Level lvl, std::string const & msg) const;
+
+    /**
+     * @param if true all unregistered components will be logged,
+     * 			if false only registered components will be logged
+     */
+    void logAll()
+    {
+    	mask = ~0;
+    }
+    
+        
+    /**
+     * @param comp : component name
+     * @return respectiv bitmask assigned to given component
+     */
+    bitmask getMask(component const & comp);
+
+private:
+
+    ///Private constructor
+    Logger();
+    // Copy constructor (not implemented)
+    Logger(Logger const &);
+    // Assignment operator (not implemented)
+    Logger & operator=(Logger const &);
+
+    /// current log level
+    short level;
+    /// number of components that were assigned with a bitmask
+    int size;
+    /// global bitmask with all registered components
+    bitmask mask;
+    /// component name to bitmask mapping
+    std::map<component, bitmask> mapping;
+
+
+
+};
+
+
+#endif
---
+++ src/utils/Logger.cpp
@@ -0,0 +1,82 @@
+#include "utils/logger.h"
+#include <stdlib.h>
+#include <iostream>
+#include <boost/iterator/iterator_concepts.hpp>
+
+Logger *Logger::instance = 0;
+Logger::bitmask Logger::unregistered = ~0;
+
+Logger::Logger() : level(DEBUG), size(0)
+{
+    mask = 0;
+    registerComponent("unregistered");
+    mask = unregistered = getMask("unregistered");
+    
+    // log the process ID, connect to syslog without delay, log also to 'cerr'
+    int options = LOG_PID | LOG_NDELAY | LOG_PERROR;
+    // setting 'ident' to NULL means that the program name will be used
+    openlog(0, options, LOG_USER);
+}
+
+
+
+Logger::~Logger()
+{
+    closelog();
+}
+
+void Logger::log(Level lvl, std::string const & msg) const
+{
+    syslog(LOG_ERR, msg.c_str());
+}
+
+void Logger::registerComponent(component const &  comp)
+{
+    // check if a mapping already exists
+    std::map<component, bitmask>::const_iterator cit = mapping.find(comp);
+    if (cit != mapping.end()) return;
+    // otherwise generate a new bitmask for the component
+    bitmask m = 0;
+    m    |= 1 << size; // mask for the component
+    mask |= 1 << size; // global mask with all components
+    ++size;
+    // store the mapping
+    mapping.insert(std::make_pair(comp, m));
+    
+    std::ostringstream outs;
+    outs << "Logger::registerComponent" << " : " << "Registered logger component" << comp << "," << size;                      			\
+    log(BASE, outs.str());
+}
+
+void Logger::registerComponents(std::vector<component> const & components)
+{
+    std::vector<component>::const_iterator it;
+    for (it = components.begin(); it != components.end(); ++it)
+    {
+        registerComponent(*it);
+    }
+}
+
+Logger::bitmask Logger::getMask(component const & comp)
+{
+    // if the component has been registered return the respective bit mask
+    std::map<component, bitmask>::const_iterator it = mapping.find(comp);
+    if (it != mapping.end()) return it->second;
+    // otherwise return the mask reserved for unregistered componentsS
+    return unregistered;
+}
+
+void Logger::setLogged(component const &comp, bool tobelogged) {
+    registerComponent(comp);
+    bitmask b = getMask(comp);
+    
+    if (tobelogged)
+      // Switch on the corresponsing bit
+      mask |= b;
+    else
+      // Switch off the corresponding bit
+      mask &= ~b;
+    
+    
+}
+
