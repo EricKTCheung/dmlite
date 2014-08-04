fa11faf818755ec5c57fdfb51e537aafd08808bd
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2014-08-04 14:37:47 +0000

    encapsulate the python exception binding in python/exceptions.cpp

--- dmlite/trunk/python/exceptions.cpp
+++ dmlite/trunk/python/exceptions.cpp
@@ -7,6 +7,29 @@
 
 #include "pydmlite.h"
 
+PyObject* createExceptionClass(const char* name, PyObject* baseTypeObj)
+{
+    using std::string;
+    namespace bp = boost::python;
+
+    string scopeName = bp::extract<string>(bp::scope().attr("__name__"));
+    string qualifiedName0 = scopeName + "." + name;
+    char* qualifiedName1 = const_cast<char*>(qualifiedName0.c_str());
+
+    PyObject* typeObj = PyErr_NewException(qualifiedName1, baseTypeObj, 0);
+    if(!typeObj) bp::throw_error_already_set();
+    bp::scope().attr(name) = bp::handle<>(bp::borrowed(typeObj));
+    return typeObj;
+}
+
+PyObject* dmExceptionTypeObj = 0;
+
+void translate(const DmException& e)
+{
+  PyObject_SetAttrString(dmExceptionTypeObj, "code", PyInt_FromLong(e.code()));
+  PyErr_SetString(dmExceptionTypeObj, e.what());
+}
+
 void export_exceptions()
 {
     class_<DmException> ("DmException", init<>())
