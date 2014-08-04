f8c4689dece8f44e0b36db48e9e5e78caa2f5143
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2014-08-04 14:37:47 +0000

    encapsulate the python exception binding in python/exceptions.cpp

--- python/pydmlite.cpp
+++ python/pydmlite.cpp
@@ -22,28 +22,10 @@ void export_io();
 void export_pooldriver();
 void export_poolmanager();
 
-PyObject* createExceptionClass(const char* name, PyObject* baseTypeObj = PyExc_Exception)
-{
-    using std::string;
-    namespace bp = boost::python;
+PyObject* createExceptionClass(const char* name, PyObject* baseTypeObj = PyExc_Exception);
+void translate(const DmException& e);
 
-    string scopeName = bp::extract<string>(bp::scope().attr("__name__"));
-    string qualifiedName0 = scopeName + "." + name;
-    char* qualifiedName1 = const_cast<char*>(qualifiedName0.c_str());
-
-    PyObject* typeObj = PyErr_NewException(qualifiedName1, baseTypeObj, 0);
-    if(!typeObj) bp::throw_error_already_set();
-    bp::scope().attr(name) = bp::handle<>(bp::borrowed(typeObj));
-    return typeObj;
-}
-
-PyObject* dmExceptionTypeObj = 0;
-
-void translate(const DmException& e)
-{
-  PyObject_SetAttrString(dmExceptionTypeObj, "code", PyInt_FromLong(e.code()));
-  PyErr_SetString(dmExceptionTypeObj, e.what());
-}
+extern PyObject* dmExceptionTypeObj;
 
 BOOST_PYTHON_MODULE(pydmlite)
 {
