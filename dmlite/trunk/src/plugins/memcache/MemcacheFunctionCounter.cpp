fbdafdd1f7574e0fcab0139ad6fc3378ab89f7b8
mhellmic <mhellmic@4525493e-7705-40b1-a816-d608a930855b>
2014-08-14 18:39:39 +0000

    change MemcacheFunctionCounter incr() printing to simplify debugging

--- dmlite/trunk/src/plugins/memcache/MemcacheFunctionCounter.cpp
+++ dmlite/trunk/src/plugins/memcache/MemcacheFunctionCounter.cpp
@@ -35,12 +35,11 @@ void MemcacheFunctionCounter::incr(const int key, unsigned int *seed)
   if (random_print_int == static_cast<unsigned int>(0)) {
     bool do_reset = false;
     std::stringstream log_stream;
-    // debug print the counters
-    boost::format log_format("%1%: %2%\n");
     {
       boost::mutex::scoped_lock lock(this->write_mutex_);
       for (int idx = 0; idx < NUM_CATALOG_API_FUNCTIONS; ++idx) {
-        log_stream << log_format % catalog_func_names[idx] % this->counter_array_[idx];
+        log_stream << catalog_func_names[idx];
+        log_stream << ": " << this->counter_array_[idx] << std::endl;
         if ((this->counter_array_[idx] - (1LL << 40)) > 0) {
           do_reset = true;
         }
