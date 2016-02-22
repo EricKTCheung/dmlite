a5b0fdbe3789f4a2ade4fd1cbd5274cc1187af78
amanzi <amanzi@4525493e-7705-40b1-a816-d608a930855b>
2016-02-22 14:51:39 +0000

    added quotatoken methods

--- dmlite/branches/dmlite_dpmrest_dev-0.7.4/src/dome/cli/executor.py
+++ dmlite/branches/dmlite_dpmrest_dev-0.7.4/src/dome/cli/executor.py
@@ -5,14 +5,16 @@ import json
 
 class DomeExecutor(object):
 	'''It invokes the method via Davix CLI'''
-	def __init__(self):
+	def __init__(self,clientDN,clientAddress):
+		self.clientDN = clientDN
+		self.clientAddress = clientAddress
 		self.baseArgs = ["davix-http", "-k","--cert", "/etc/grid-security/hostcert.pem","--key", "/etc/grid-security/hostkey.pem"]
-	def putDone(self, url,lfn, clientDN, clientAddress,pfn,server):
+	def putDone(self, url,lfn,pfn,server):
         	args = self.baseArgs
                 args.append("-X POST")
 		url = url + lfn
 		args.append(url)
-		args = self.addClient(args,clientDN ,clientAddress)
+		args = self.addClient(args)
 		args.append("-H")
 		args.append(quote('cmd: dome_putdone'))
 		args.append("--data")
@@ -21,30 +23,30 @@ class DomeExecutor(object):
 		data['server'] = server
 		args.append(quote(json.dumps(data)))
 		self.executeDavix(args)
-	def put(self,url,lfn, clientDN, clientAddress):
+	def put(self,url,lfn):
 		args = self.baseArgs
 		args.append("-X PUT")
 		url = url + lfn
 		args.append(url)
-		args = self.addClient(args,clientDN ,clientAddress)
+		args = self.addClient(args)
 		args.append("--data")
 		args.append(quote("{}"))
 		self.executeDavix(args)
-	def getSpaceInfo(self,url, clientDN, clientAddress):
+	def getSpaceInfo(self,url):
 		args = self.baseArgs
 		args.append("-X GET")
 		args.append(url+"/dome")
-		args = self.addClient(args,clientDN ,clientAddress)
+		args = self.addClient(args)
 		args.append("-H")
 	 	args.append(quote('cmd: dome_getspaceinfo'))
 		args.append("--data")
                 args.append(quote("{}"))
                 self.executeDavix(args)
-	def statPool(self,url, pool, clientDN, clientAddress):
+	def statPool(self,url, pool):
 		args = self.baseArgs
                 args.append("-X GET")
                 args.append(url+"/dome")
-                args = self.addClient(args,clientDN ,clientAddress)
+                args = self.addClient(args)
                 args.append("-H")
                 args.append(quote('cmd: dome_statpool'))
                 args.append("--data")
@@ -52,15 +54,43 @@ class DomeExecutor(object):
 		data['poolname']=pool
                 args.append(quote(json.dumps(data)))
                 self.executeDavix(args)
+	def getquotatoken(self,url,lfn):
+		args = self.baseArgs
+		args.append("-X GET")
+		url = url + lfn
+                args.append(url)
+		args = self.addClient(args)
+		args.append("-H")
+                args.append(quote('cmd: dome_getquotatoken'))
+                args.append("--data")
+                args.append(quote("{}"))
+                self.executeDavix(args)
+	def setquotatoken(self,url,lfn,pool, space,desc):
+                args = self.baseArgs
+	        args.append("-X POST")
+ 		url = url + "/dome" + lfn
+		args.append(url)
+                args = self.addClient(args)
+		args.append("-H")
+                args.append(quote('cmd: dome_setquotatoken'))
+	        args.append("--data")
+                data = {}
+                data['path']=lfn
+                data['poolname'] = pool
+		data['quotaspace'] = space
+		data['description']=desc
+                args.append(quote(json.dumps(data)))
+                self.executeDavix(args)
+
 	def executeDavix(self,args):
 		args = (' ').join(args)
 		print args
                 proc = Popen(args, shell=True,stdout=PIPE)
 		out = proc.communicate()[0]
                 print out
-	def addClient(self,args,clientDN,clientAddress):
+	def addClient(self,args):
                 args.append("-H")
-                args.append(quote("remoteclientdn: "+clientDN))
+                args.append(quote("remoteclientdn: "+self.clientDN))
                 args.append("-H")
-                args.append(quote("remoteclientaddr: "+clientAddress))
+                args.append(quote("remoteclientaddr: "+self.clientAddress))
 		return args
