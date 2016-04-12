7719826167c6c0e9bcaed45a92d0a8ead60dd655
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2016-04-12 14:40:22 +0000

    Avoid thread-unsafe extendedstat() call before creating a new file Return EEXIST on mysql duplicate key error

--- src/plugins/mysql/NsMySql.cpp
+++ src/plugins/mysql/NsMySql.cpp
@@ -226,13 +226,13 @@ ExtendedStat INodeMySql::create(const ExtendedStat& nf) throw (DmException)
   // Destination must not exist!
   // For the future... I think that this overhead can be avoided by more carefully
   // checking for the execute() return values and exceptions
-  try {
-    this->extendedStat(nf.parent, nf.name);
-    throw DmException(EEXIST, "%s already exists", nf.name.c_str());
-  }
-  catch (DmException& e) {
-    if (e.code() != ENOENT) throw;
-  }
+//   try {
+//     this->extendedStat(nf.parent, nf.name);
+//     throw DmException(EEXIST, "%s already exists", nf.name.c_str());
+//   }
+//   catch (DmException& e) {
+//     if (e.code() != ENOENT) throw;
+//   }
 
   
     
@@ -281,6 +281,7 @@ ExtendedStat INodeMySql::create(const ExtendedStat& nf) throw (DmException)
   // Start a new transaction
   InodeMySqlTrans trans(this);
   
+  try {
   {
     
     
@@ -333,7 +334,15 @@ ExtendedStat INodeMySql::create(const ExtendedStat& nf) throw (DmException)
   // Commit the local trans object
   // This also releases the connection back to the pool
   trans.Commit();
-  
+  }
+  catch (DmException e) {
+    if (e.code() | DMLITE_DATABASE_ERROR) {
+      int c = e.code() ^ DMLITE_DATABASE_ERROR;
+      if (c == 1062) // mysql duplicate key
+        throw DmException(EEXIST, "File already exists - mysql duplicate key");
+    }
+    throw;
+  }
 
   Log(Logger::Lvl3, mysqllogmask, mysqllogname, "Exiting.");
   
