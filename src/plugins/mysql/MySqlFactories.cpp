6a6cb667c10659a484cc61700f0caec214a4d236
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2016-04-12 13:43:49 +0000

    Create a new idcard plugin_mysql_nsonly that instantiates only the ns part

--- src/plugins/mysql/MySqlFactories.cpp
+++ src/plugins/mysql/MySqlFactories.cpp
@@ -347,7 +347,7 @@ IODriver* MysqlIOPassthroughFactory::createIODriver(PluginManager* pm) throw (Dm
 // Static functions implementing hooks for the dynamic loading
 //
 
-// Register plugin item that gives name space functionalities
+// Register plugin item that gives name space functionalities together with authentication
 static void registerPluginNs(PluginManager* pm) throw(DmException)
 {
   mysqllogmask = Logger::get()->getMask(mysqllogname);
@@ -357,6 +357,15 @@ static void registerPluginNs(PluginManager* pm) throw(DmException)
   pm->registerAuthnFactory(nsFactory);
 }
 
+// Register plugin item that gives name space functionalities only
+static void registerPluginNsOnly(PluginManager* pm) throw(DmException)
+{
+  mysqllogmask = Logger::get()->getMask(mysqllogname);
+  Log(Logger::Lvl4, mysqllogmask, mysqllogname, "registerPluginNsOnly");
+  NsMySqlFactory* nsFactory = new NsMySqlFactory();
+  pm->registerINodeFactory(nsFactory);
+
+}
 
 // Register plugin item that gives disk pool manager functionalities
 static void registerPluginDpm(PluginManager* pm) throw(DmException)
@@ -387,6 +396,11 @@ static void registerPluginMysqlIOPassthrough(PluginManager* pm) throw(DmExceptio
 PluginIdCard plugin_mysql_ns = {
   PLUGIN_ID_HEADER,
   registerPluginNs
+};
+
+PluginIdCard plugin_mysql_nsonly = {
+  PLUGIN_ID_HEADER,
+  registerPluginNsOnly
 };
 
 PluginIdCard plugin_mysql_dpm = {
