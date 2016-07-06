a2b602c3801018abe8a3f273872a72270c67dd62
gbitzes <gbitzes@4525493e-7705-40b1-a816-d608a930855b>
2016-07-06 14:43:03 +0000

    Add/remove forgotten files, update deserializePool to handle pool without filesystems

--- src/plugins/domeadapter/DomeAdapterUtils.h
+++ src/plugins/domeadapter/DomeAdapterUtils.h
@@ -19,25 +19,25 @@ inline dmlite::Pool deserializePool(boost::property_tree::ptree::const_iterator 
     p["s_type"] = it->second.get<std::string>("s_type", "");
     p["defsize"] = it->second.get<uint64_t>("defsize", 0);
 
-
-
     // fetch info about the filesystems
     std::vector<boost::any> filesystems;
 
-    ptree fsinfo = it->second.get_child("fsinfo");
+    if(it->second.count("fsinfo") != 0) {
+      ptree fsinfo = it->second.get_child("fsinfo");
 
-    // iterating over servers
-    for(ptree::const_iterator it2 = fsinfo.begin(); it2 != fsinfo.end(); it2++) {
-      // iterating over filesystems
-      for(ptree::const_iterator it3 = it2->second.begin(); it3 != it2->second.end(); it3++) {
-        Extensible fs;
-        fs["server"] = it2->first;
-        fs["fs"] = it3->first;
-        fs["status"] = it3->second.get<uint64_t>("fsstatus", 0);
-        fs["freespace"] = it3->second.get<uint64_t>("freespace", 0);
-        fs["physicalsize"] = it3->second.get<uint64_t>("physicalsize", 0);
+      // iterating over servers
+      for(ptree::const_iterator it2 = fsinfo.begin(); it2 != fsinfo.end(); it2++) {
+        // iterating over filesystems
+        for(ptree::const_iterator it3 = it2->second.begin(); it3 != it2->second.end(); it3++) {
+          Extensible fs;
+          fs["server"] = it2->first;
+          fs["fs"] = it3->first;
+          fs["status"] = it3->second.get<uint64_t>("fsstatus", 0);
+          fs["freespace"] = it3->second.get<uint64_t>("freespace", 0);
+          fs["physicalsize"] = it3->second.get<uint64_t>("physicalsize", 0);
 
-        filesystems.push_back(fs);
+          filesystems.push_back(fs);
+        }
       }
     }
 
