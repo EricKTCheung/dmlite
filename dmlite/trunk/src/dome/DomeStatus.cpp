528ee3adcbb6e746cf81f89cf6c642142fe8e38b
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2016-08-03 12:09:50 +0000

    Improve dome_pull with the desired free space

--- dmlite/trunk/src/dome/DomeStatus.cpp
+++ dmlite/trunk/src/dome/DomeStatus.cpp
@@ -447,15 +447,30 @@ void DomeStatus::tickFilepulls() {
     sec.credentials.clientName = qualifiers[5];
     sec.credentials.remoteAddress = qualifiers[6];
 
+    
+    // Try getting the default space for the pool
+    int64_t pool_defsize;
+    char pool_stype;
+    DomeFsInfo fsinfo;
+    if(!PfnMatchesAnyFS(server, fs, fsinfo)) {
+      Err("dome_pull", SSTR("pfn does not match any of the filesystems of this server."));
+      continue;
+    }
+    if (!getPoolInfo(fsinfo.poolname, pool_defsize, pool_stype)) {
+      Err("dome_pull", SSTR("Can't get pool for fs: '" << fsinfo.server << ":" << fsinfo.fs));
+      continue;
+    }
+    
     // send pull command to the disk to initiate calculation
     Log(Logger::Lvl1, domelogmask, domelogname, "Contacting disk server " << server << " for pulling '" << rfn << "'");
     std::string diskurl = "https://" + server + "/domedisk/";
 
 
+    
     DomeTalker talker(*davixPool, &sec, diskurl,
                       "POST", "dome_pull");
 
-    if(!talker.execute("lfn", lfn, "pfn", DomeUtils::pfn_from_rfio_syntax(rfn))) {
+    if(!talker.execute("lfn", lfn, "pfn", DomeUtils::pfn_from_rfio_syntax(rfn)), "neededspace", pool_defsize) {
       Err(domelogname, "ERROR when issuing dome_pull to diskserver: " << talker.err());
       continue;
     }
