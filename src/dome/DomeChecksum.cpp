6d17d1f8806c63f4f8c8eadc58e537158513d84b
gbitzes <gbitzes@4525493e-7705-40b1-a816-d608a930855b>
2016-08-11 14:06:38 +0000

    Add support for 'checksum.'-prefixed types to dome-checksum

--- src/dome/DomeChecksum.cpp
+++ src/dome/DomeChecksum.cpp
@@ -45,7 +45,7 @@ int zlib_adler32(string checksumtype, int fd) {
 
   uLong adler = adler32(0L, Z_NULL, 0);
   while( (nbytes = read(fd, buffer, sizeof(buffer))) != 0) {
-    adler = adler32(adler, buffer, nbytes);    
+    adler = adler32(adler, buffer, nbytes);
   }
 
   unsigned char output[4];
@@ -53,7 +53,7 @@ int zlib_adler32(string checksumtype, int fd) {
   output[2] = ((adler >> CHAR_BIT) & 0xFF);
   output[1] = ((adler >> (CHAR_BIT * 2)) & 0xFF);
   output[0] = ((adler >> (CHAR_BIT * 3)) & 0xFF);
- 
+
   cout << ">>>>> HASH " << hexPrinter(output, 4) << endl;
   return 0;
 }
@@ -114,7 +114,15 @@ int openssl_sha224(string checksumtype, int fd) {
   return 0;
 }
 
+std::string remove_prefix_if_exists(std::string str, std::string prefix) {
+    if(prefix.size() > str.size()) return str;
 
+    if(std::equal(prefix.begin(), prefix.end(), str.begin())) {
+      return str.substr(prefix.size(), str.size()-prefix.size());
+    }
+
+    return str;
+}
 
 int main(int argc, char** argv) {
   if(argc != 3) {
@@ -123,7 +131,7 @@ int main(int argc, char** argv) {
     return EINVAL;
   }
 
-  string checksumtype = argv[1];
+  string checksumtype = remove_prefix_if_exists(argv[1], "checksum.");
   string filename = argv[2];
 
   // make sure I can read the file
@@ -134,7 +142,7 @@ int main(int argc, char** argv) {
   }
 
   int ret;
-  
+
   if(checksumtype == "crc32") ret = zlib_crc32(checksumtype, fd);
   else if(checksumtype == "adler32") ret = zlib_adler32(checksumtype, fd);
   else if(checksumtype == "md5") ret = openssl_md5(checksumtype, fd);
