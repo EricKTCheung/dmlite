138c87833a73923b487f68eba54d2c23a769a6e7
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2016-11-09 13:33:49 +0000

    Now dome_addpool does not reject pooltypes anymore

--- dmlite/trunk/src/dome/DomeCoreXeq.cpp
+++ dmlite/trunk/src/dome/DomeCoreXeq.cpp
@@ -2899,7 +2899,7 @@ int DomeCore::dome_addpool(DomeReq &req, FCGX_Request &request) {
   if (pool_defsize < 1024*1024) {
     return DomeReq::SendSimpleResp(request, 422, SSTR("Invalid defsize: " << pool_defsize));
   }
-  if(pool_stype != "P" || pool_stype != "V") {
+  if(pool_stype != "P" && pool_stype != "V") {
     return DomeReq::SendSimpleResp(request, 422, SSTR("Invalid pool_stype: " << pool_stype));
   }
 
