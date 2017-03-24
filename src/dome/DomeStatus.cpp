468b30e5ce26797b3a02d482d9186b9515af9e34
furano <furano@4525493e-7705-40b1-a816-d608a930855b>
2017-03-24 09:40:40 +0000

    A filesystem can pull a file if its physical size is larger than defsize

--- src/dome/DomeStatus.cpp
+++ src/dome/DomeStatus.cpp
@@ -57,7 +57,9 @@ bool DomeFsInfo::canPullFile(DomeStatus &st) {
   char pooltype;
   int64_t defsz;
   st.getPoolInfo(poolname, defsz, pooltype);
-  return ( ((pooltype == 'V') || (pooltype == 'v')) && (freespace > defsz) );
+//  return ( ((pooltype == 'V') || (pooltype == 'v')) && (freespace > defsz) );
+  return ( ((pooltype == 'V') || (pooltype == 'v')) && (physicalsize > defsz) );
+
 }
 
 DomeStatus::DomeStatus() {
